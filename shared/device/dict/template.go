ad71fcd39ed1be9b6e078cb512c08c343fed87fb
godLei6 <603785348@qq.com>
2021-06-27 22:52:00 +0800

    更新方式,暂未完成

--- shared/device/dict/template.go
+++ shared/device/dict/template.go
@@ -182,3 +182,27 @@ func NewTemplate(templateStr []byte)(*Template,error){
 	template.init()
 	return &template, nil
 }
+
+type TempParam struct {
+	ID       string `json:"id"`       //标识符
+	Name     string `json:"name"`     //功能名称
+	Desc     string `json:"gesc"`     //描述
+	Mode     string `json:"mode"`     //读写乐行:rw(可读可写) r(只读)
+	Required bool   `json:"required"` //是否必须
+	Type     string  `json:"type"`     //事件类型: 信息:info  告警alert  故障:fault
+	Value   struct{
+		Type      string            `json:"type"`                //参数类型:bool int string struct float timestamp array enum
+		Maping    map[string]string `json:"mapping,omitempty"`   //枚举及bool类型:bool enum
+		Min       string            `json:"min,omitempty"`       //数值最小值:int string float
+		Max       string            `json:"max,omitempty"`       //数值最大值:int string float
+		Start     string            `json:"start,omitempty"`     //初始值:int float
+		Step      string            `json:"step,omitempty"`      //步长:int float
+		Unit      string            `json:"unit,omitempty"`      //单位:int float
+		Value      *TempParam `json:"Value"`
+	} `json:"Value"`   //数据定义
+}
+
+func (t *TempParam)AddDefine(define Define)  {
+	t.Value.Type = define.Type
+	//todo 
+}
