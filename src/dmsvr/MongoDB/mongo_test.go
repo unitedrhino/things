a4c4f5d7f93bb59e07fac790461b7f4e9c748eac
yanglei <yanglei2@richinfo.cn>
2021-06-29 20:50:15 +0800

    添加MongoDB 操作测试代码

---
+++ src/dmsvr/MongoDB/mongo_test.go
@@ -0,0 +1,41 @@
+package MongoDB
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"testing"
+)
+
+func TestMongo(t *testing.T) {
+	clientOpt := options.Client().ApplyURI(mongoUrl)
+	client, err := mongo.Connect(context.TODO(), clientOpt)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	}
+
+	// 检查连接情况
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	//指定要操作的数据集
+	collection := client.Database("things").Collection("test3")
+
+	//执行增删改查操作
+	insertSensor(client, collection)
+
+	querySensor(collection, "")
+	// 断开客户端连接
+	err = client.Disconnect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection to MongoDB closed.")
+}
