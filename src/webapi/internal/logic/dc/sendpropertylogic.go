d832cedc830601a194a843db7f4ea31f138f7a60
yangl30 <yangl30@mingyuanyun.com>
2021-11-09 20:36:26 +0800

    完善dc的属性控制功能

--- src/webapi/internal/logic/dc/sendpropertylogic.go
+++ src/webapi/internal/logic/dc/sendpropertylogic.go
@@ -2,6 +2,9 @@ package dc
 
 import (
 	"context"
+	"gitee.com/godLei6/things/shared/errors"
+	"gitee.com/godLei6/things/shared/utils"
+	"gitee.com/godLei6/things/src/dcsvr/dc"
 
 	"gitee.com/godLei6/things/src/webapi/internal/svc"
 	"gitee.com/godLei6/things/src/webapi/internal/types"
@@ -24,7 +27,25 @@ func NewSendPropertyLogic(ctx context.Context, svcCtx *svc.ServiceContext) SendP
 }
 
 func (l *SendPropertyLogic) SendProperty(req types.SendDcPropertyReq) (*types.SendDcPropertyResp, error) {
-	// todo: add your logic here and delete this line
-
-	return &types.SendDcPropertyResp{}, nil
+	l.Infof("SendProperty|req=%+v", req)
+	resp,err := l.svcCtx.DcRpc.SendProperty(l.ctx,&dc.SendPropertyReq{
+		MemberID:             req.MemberID,
+		MemberType:           req.MemberType,
+		ProductID:            req.ProductID,
+		DeviceName:           req.DeviceName,
+		Data:				req.Data,
+		DataTimestamp:req.DataTimestamp,
+		Method:req.Method,
+	})
+	if err != nil {
+		er := errors.Fmt(err)
+		l.Errorf("%s|rpc.SendProperty|req=%v|err=%+v", utils.FuncName(), req, er)
+		return nil, er
+	}
+	return &types.SendDcPropertyResp{
+		Data:resp.Data,
+		ClientToken:resp.ClientToken,  //调用id
+		Status:resp.Status,       //返回状态
+		Code:resp.Code,  //设备返回状态码
+	}, nil
 }
