037bfd3396e4b2ac822593f6a9ae8b9a3aa537b7
godLei6 <603785348@qq.com>
2022-04-14 21:54:53 +0800

    dev: 新增属性数据插入

--- src/dmsvr/internal/repo/tdengine/deviceData.go
+++ src/dmsvr/internal/repo/tdengine/deviceData.go
@@ -10,6 +10,7 @@ import (
 	"github.com/i-Things/things/src/dmsvr/internal/domain/deviceTemplate"
 	"github.com/zeromicro/go-zero/core/logx"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -33,14 +34,70 @@ func (d *DeviceDataRepo) InsertEventData(ctx context.Context, productID string,
 		return errors.System.AddDetail("param json parse failure")
 	}
 	sql := fmt.Sprintf("insert into %s (ts,event_id,event_type, param) values (?,?,?,?);", getEventTableName(productID, deviceName))
-	if _, err := d.t.Exec(sql, event.TimeStamp, event.ID, event.Type, string(param)); err != nil {
+	if _, err := d.t.Exec(sql, event.TimeStamp, event.ID, event.Type, param); err != nil {
 		return err
 	}
 	return nil
 }
 
+// GenParams 返回占位符?,?,?,? 参数id名:aa,bbb,ccc 参数值列表
+func (d *DeviceDataRepo) GenParams(params map[string]interface{}) (string, string, []interface{}, error) {
+	if len(params) == 0 {
+		//使用这个函数前必须要判断参数的个数是否大于0
+		panic("DeviceDataRepo|GenParams|params num == 0")
+	}
+	var (
+		paramPlaceholder = strings.Repeat("?,", len(params))
+		paramValList     []interface{} //参数值列表
+		paramIds         []string
+	)
+	//将最后一个?去除
+	paramPlaceholder = paramPlaceholder[:len(paramPlaceholder)-1]
+	for k, v := range params {
+		paramIds = append(paramIds, k)
+		if _, ok := v.([]interface{}); !ok {
+			paramValList = append(paramValList, v)
+		} else { //如果是数组类型,需要序列化为json
+			param, err := json.Marshal(v)
+			if err != nil {
+				return "", "", nil, errors.System.AddDetail("param json parse failure")
+			}
+			paramValList = append(paramValList, param)
+		}
+	}
+	return paramPlaceholder, strings.Join(paramIds, ","), paramValList, nil
+}
+
 func (d *DeviceDataRepo) InsertPropertyData(ctx context.Context, productID string, deviceName string, property *deviceTemplate.PropertyData) error {
-	panic("implement me")
+	switch property.Param.(type) {
+	case map[string]interface{}: //结构体类型
+		paramPlaceholder, paramIds, paramValList, err := d.GenParams(property.Param.(map[string]interface{}))
+		if err != nil {
+			return err
+		}
+		sql := fmt.Sprintf("insert into %s (ts, %s) values (?,%s);",
+			getPropertyTableName(productID, deviceName, property.ID), paramIds, paramPlaceholder)
+		param := append([]interface{}{property.TimeStamp}, paramValList...)
+		if _, err := d.t.Exec(sql, param...); err != nil {
+			return err
+		}
+	default:
+		var (
+			param = property.Param
+			err   error
+		)
+		if _, ok := property.Param.([]interface{}); ok { //如果是数组类型,需要先序列化为json
+			param, err = json.Marshal(property.Param)
+			if err != nil {
+				return errors.System.AddDetail("param json parse failure")
+			}
+		}
+		sql := fmt.Sprintf("insert into %s (ts, param) values (?,?);", getPropertyTableName(productID, deviceName, property.ID))
+		if _, err := d.t.Exec(sql, property.TimeStamp, param); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (d *DeviceDataRepo) InsertPropertiesData(ctx context.Context, productID string, deviceName string, params map[string]interface{}, timestamp time.Time) error {
