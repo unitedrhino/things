a3f13f58dfe1416916903c1831baa620392d78fe
godLei6 <603785348@qq.com>
2022-05-25 23:12:53 +0800

    dev: 一波更新 - 调试通过将设备调试日志,设备远程配置,设备影子,ota升级融入到ddsvr中并调试通过 - 将ddsvr从rpc服务修改为http服务以便支持设备以http,ws的方式上报消息

--- src/ddsvr/internal/logic/pinglogic.go
+++ src/ddsvr/internal/logic/pinglogic.go
@@ -4,28 +4,25 @@ import (
 	"context"
 
 	"github.com/i-Things/things/src/ddsvr/internal/svc"
-	"github.com/i-Things/things/src/ddsvr/pb/dd"
-
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
 type PingLogic struct {
+	logx.Logger
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
-	logx.Logger
 }
 
 func NewPingLogic(ctx context.Context, svcCtx *svc.ServiceContext) *PingLogic {
 	return &PingLogic{
+		Logger: logx.WithContext(ctx),
 		ctx:    ctx,
 		svcCtx: svcCtx,
-		Logger: logx.WithContext(ctx),
 	}
 }
 
-// ping pong 测试
-func (l *PingLogic) Ping(in *dd.Empty) (*dd.Empty, error) {
+func (l *PingLogic) Ping() error {
 	// todo: add your logic here and delete this line
 
-	return &dd.Empty{}, nil
+	return nil
 }
