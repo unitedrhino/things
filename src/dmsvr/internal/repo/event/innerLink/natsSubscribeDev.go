fe8dea171fca0456f8cdcb7bf5974546b7ed4e3f
lbs0019873800lbs <lbs_mike2006@163.com>
2022-06-01 22:24:36 +0800

    dev：去掉多余库的别名

--- src/dmsvr/internal/repo/event/innerLink/natsSubscribeDev.go
+++ src/dmsvr/internal/repo/event/innerLink/natsSubscribeDev.go
@@ -2,7 +2,7 @@ package innerLink
 
 import (
 	"github.com/i-Things/things/shared/events"
-	my_trace "github.com/i-Things/things/shared/traces"
+	"github.com/i-Things/things/shared/traces"
 	"github.com/i-Things/things/src/dmsvr/internal/domain/device"
 	"github.com/nats-io/nats.go"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -36,7 +36,7 @@ func (s *SubDev) GetMsg(timeout time.Duration) (ele *device.PublishMsg, err erro
 	}
 	ctx := emsg.GetCtx()
 	//向jaeger推送当前节点信息，路径名为主题名
-	ctx, span := my_trace.StartSpan(ctx, msg.Subject, "")
+	ctx, span := traces.StartSpan(ctx, msg.Subject, "")
 	logx.Infof("[mqtt.GetMsg]|-------------------trace:%s, spanid:%s|topic:%s",
 		span.SpanContext().TraceID(), span.SpanContext().SpanID(), msg.Subject)
 	defer span.End()
