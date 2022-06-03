24b0691ef20ffa7b9c5dc920382ddb2e4b83daa1
lbs0019873800lbs <lbs_mike2006@163.com>
2022-06-03 22:52:03 +0800

    dev:链路追踪的实现

--- shared/events/events.go
+++ shared/events/events.go
@@ -3,14 +3,23 @@ package events
 import (
 	"context"
 	"encoding/json"
+	"github.com/zeromicro/go-zero/core/logx"
+	"go.opentelemetry.io/otel/trace"
 	"time"
 )
 
+type MySpanContextConfig struct {
+	TraceID string
+	SpanID  string
+	//TraceFlags byte
+	//TraceState string
+	//Remote     bool
+}
 type (
 	// MsgHead 消息队列的头
 	//todo 后续考虑用proto重构这个头
 	MsgHead struct {
-		Trace     string //追踪tid
+		Trace     []byte //追踪tid
 		Timestamp int64  //发送时毫秒级时间戳
 		Data      []byte //传送的内容
 	}
@@ -23,8 +32,12 @@ type (
 )
 
 func NewEventMsg(ctx context.Context, data []byte) []byte {
+	//生成新的消息时，使用go-zero的链路追踪接口，从ctx中提取span信息，并放入MsgHead中的Trace字段
+	span := trace.SpanFromContext(ctx)
+	traceinfo, err := span.SpanContext().MarshalJSON()
+
 	msg := MsgHead{
-		Trace:     "",
+		Trace:     traceinfo,
 		Timestamp: time.Now().UnixMilli(),
 		Data:      data,
 	}
@@ -45,8 +58,35 @@ func GetEventMsg(data []byte) EventHandle {
 }
 
 func (m *MsgHead) GetCtx() context.Context {
-	//todo 等待实现链路追踪
-	return context.Background()
+	var msg MySpanContextConfig
+	err := json.Unmarshal(m.Trace, &msg)
+	if err != nil {
+		logx.Error("[GetCtx]|json Unmarshal trace.SpanContextConfig err.")
+		return nil
+	}
+	//将MsgHead 中的msg链路信息 重新注入ctx中并返回
+	t, err := trace.TraceIDFromHex(msg.TraceID)
+	if err != nil {
+		logx.Error("[GetCtx]|TraceIDFromHex err.")
+		return nil
+	}
+	s, err := trace.SpanIDFromHex(msg.SpanID)
+	if err != nil {
+		logx.Error("[GetCtx]|SpanIDFromHex err.")
+		return nil
+	}
+	parent := trace.NewSpanContext(trace.SpanContextConfig{
+		TraceID: t,
+		SpanID:  s,
+		//TraceFlags: msg.TraceFlags,
+		//raceState:  msg.TraceState,
+		//Remote:     msg.Remote,
+	})
+
+	/*	ctx, span := my_trace.StartSpan(ctx, "", "")
+		span.End()*/
+
+	return trace.ContextWithRemoteSpanContext(context.Background(), parent)
 }
 
 func (m *MsgHead) GetTs() time.Time {
--- src/ddsvr/internal/repo/event/innerLink/nats.go
+++ src/ddsvr/internal/repo/event/innerLink/nats.go
@@ -8,7 +8,9 @@ import (
 	"github.com/i-Things/things/shared/devices"
 	"github.com/i-Things/things/shared/events"
 	"github.com/i-Things/things/shared/events/topics"
+	"github.com/i-Things/things/shared/traces"
 	"github.com/nats-io/nats.go"
+	"github.com/zeromicro/go-zero/core/logx"
 )
 
 type (
@@ -85,6 +87,11 @@ func (n *NatsClient) Subscribe(handle Handle) error {
 	_, err := n.client.QueueSubscribe(topics.DeviceDownAll, ThingsDDDeliverGroup,
 		events.NatsSubscription(func(ctx context.Context, msg []byte) error {
 			topic, payload := devices.GetPublish(msg)
+			//给设备回包之前，将链路信息span推送至jaeger
+			_, span := traces.StartSpan(ctx, topics.DeviceDownAll, "")
+			logx.Infof("[ddsvr.mqtt.SubScribe]|-------------------trace:%s, spanid:%s|topic:%s",
+				span.SpanContext().TraceID(), span.SpanContext().SpanID(), topics.DeviceDownAll)
+			defer span.End()
 			return handle(ctx).PublishToDev(topic, payload)
 		}))
 	return err
