f36c6ce4182667bc725c7084a50cdd43e839eec0
godLei6 <603785348@qq.com>
2022-08-10 23:49:47 +0800

    dev: 补充未上传的文件

---
+++ src/apisvr/internal/domain/userHeader/define.go
@@ -0,0 +1,9 @@
+package userHeader
+
+const (
+	GUID         string = "fmcs-guid"
+	UserToken    string = "fmcs-token"
+	UserSetToken string = "fmcs-set-token"
+	UserIp       string = "fmcs-ip"
+	UserUid      string = "fmcs-uid"
+)
---
+++ src/apisvr/internal/logic/system/user/assemble.go
@@ -0,0 +1,63 @@
+package user
+
+import (
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"github.com/i-Things/things/src/apisvr/internal/types"
+	"github.com/i-Things/things/src/usersvr/pb/user"
+)
+
+func GetNullVal(val *wrappers.StringValue) *string {
+	if val == nil {
+		return nil
+	}
+	return &val.Value
+}
+
+func UserCoreToApi(core *user.UserCore) *types.UserCore {
+	return &types.UserCore{
+		Uid:         core.Uid,
+		UserName:    core.UserName,
+		Email:       core.Email,
+		Phone:       core.Phone,
+		Wechat:      core.Wechat,
+		LastIP:      core.LastIP,
+		RegIP:       core.RegIP,
+		CreatedTime: core.CreatedTime,
+		Status:      core.Status,
+		Role:        core.Role,
+	}
+}
+
+func UserInfoToApi(ui *user.UserInfo) *types.UserInfo {
+	return &types.UserInfo{
+		Uid:        ui.Uid,
+		UserName:   ui.UserName,
+		NickName:   ui.NickName,
+		InviterUid: ui.InviterUid,
+		InviterId:  ui.InviterId,
+		Sex:        ui.Sex,
+		City:       ui.City,
+		Country:    ui.Country,
+		Province:   ui.Province,
+		Language:   ui.Language,
+		HeadImgUrl: ui.HeadImgUrl,
+		CreateTime: ui.CreateTime,
+	}
+}
+
+//func UserInfoFullToApi(ui *user.UserInfo) *types.UserIndexResp {
+//	return &types.UserIndexResp{
+//		Uid:        ui.Uid,
+//		UserName:   ui.UserName,
+//		NickName:   ui.NickName,
+//		InviterUid: ui.InviterUid,
+//		InviterId:  ui.InviterId,
+//		Sex:        ui.Sex,
+//		City:       ui.City,
+//		Country:    ui.Country,
+//		Province:   ui.Province,
+//		Language:   ui.Language,
+//		HeadImgUrl: ui.HeadImgUrl,
+//		CreateTime: ui.CreateTime,
+//	}
+//}
