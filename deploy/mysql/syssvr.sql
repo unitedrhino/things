3518b23ea56f1d6a5b4116d891d8b711958d2b35
godLei6 <603785348@qq.com>
2022-09-04 17:11:03 +0800

    dev: 整理数据库sql及删除冗余代码

--- deploy/mysql/syssvr.sql
+++ deploy/mysql/syssvr.sql
@@ -2,36 +2,38 @@ create database if not EXISTS things_sys;
 use things_sys;
 CREATE TABLE if not exists `user_info`
 (
-    `uid`         bigint   NOT NULL COMMENT '用户id',
-    `userName`    varchar(255) DEFAULT NULL COMMENT '登录用户名',
-    `password`    varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '登录密码',
-    `email`       varchar(255) DEFAULT NULL COMMENT '邮箱',
-    `phone`       varchar(255) DEFAULT NULL COMMENT '手机号',
-    `wechat`      varchar(255) DEFAULT NULL COMMENT '微信union id',
-    `lastIP`      varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '最后登录ip',
-    `regIP`       varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '注册ip',
-    `nickName`    varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户的昵称',
-    `sex`         int           NOT NULL DEFAULT '3' COMMENT '用户的性别，值为1时是男性，值为2时是女性，其他值为未知',
-    `city`        varchar(256)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户所在城市',
-    `country`     varchar(256)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户所在国家',
-    `province`    varchar(256)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户所在省份',
-    `language`    varchar(256)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户的语言，简体中文为zh_CN',
-    `headImgUrl`  varchar(256)  CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户头像',
-    `role`        int not null COMMENT '用户角色',
-    `createdTime` datetime not NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updatedTime` datetime  NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deletedTime` datetime DEFAULT NULL COMMENT '删除时间，默认为空，表示未删除，非空表示已删除',
+    `uid`         bigint                                                  NOT NULL COMMENT '用户id',
+    `userName`    varchar(20)                                                      DEFAULT NULL COMMENT '登录用户名',
+    `password`    char(32) CHARACTER SET utf8 COLLATE utf8_general_ci     NOT NULL DEFAULT '' COMMENT '登录密码',
+    `email`       varchar(255)                                                     DEFAULT NULL COMMENT '邮箱',
+    `phone`       varchar(20)                                                      DEFAULT NULL COMMENT '手机号',
+    `wechat`      varchar(20)                                                      DEFAULT NULL COMMENT '微信union id',
+    `lastIP`      varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '' COMMENT '最后登录ip',
+    `regIP`       varchar(40) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '' COMMENT '注册ip',
+    `nickName`    varchar(60) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '' COMMENT '用户的昵称',
+    `sex`         tinyint(1)                                              NOT NULL DEFAULT '3' COMMENT '用户的性别，值为1时是男性，值为2时是女性，其他值为未知',
+    `city`        varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '' COMMENT '用户所在城市',
+    `country`     varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '' COMMENT '用户所在国家',
+    `province`    varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '' COMMENT '用户所在省份',
+    `language`    varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '' COMMENT '用户的语言，简体中文为zh_CN',
+    `headImgUrl`  varchar(256) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '' COMMENT '用户头像',
+    `role`        bigint                                                  not null COMMENT '用户角色',
+    `createdTime` datetime                                                not NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updatedTime` datetime                                                NULL     DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deletedTime` datetime                                                         DEFAULT NULL COMMENT '删除时间，默认为空，表示未删除，非空表示已删除',
     PRIMARY KEY (`uid`) USING BTREE,
     UNIQUE KEY `user_username` (`userName`) USING BTREE,
     UNIQUE KEY `user_phone` (`phone`) USING BTREE,
     UNIQUE KEY `user_email` (`email`) USING BTREE,
     UNIQUE KEY `user_wechat` (`wechat`) USING BTREE,
-    KEY `user_inviterUid` (`inviterUid`) USING BTREE,
     KEY `user_deletedTime` (`deletedTime`) USING BTREE
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT COMMENT='用户登录信息表';
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  ROW_FORMAT = COMPACT COMMENT ='用户登录信息表';
 
 # 新增root用户
-INSERT INTO `user_info`(`uid`, `userName`, `password`, `email`, `phone`, `wechat`, `lastIP`, `regIP`, `role`,`nickName`, `inviterUid`, `inviterId`,
-                        `sex`, `city`, `country`, `province`,`language`, `headImgUrl`,`deletedTime`)
-VALUES (1740358057038188544, 'administrator', '4f0fded4a38abe7a3ea32f898bb82298', '163', '13911110000', 'wechat', '0.0.0.0', '0.0.0.0', 1,'liangjuan',
-        4, 0x3639, 1, 'shenzhen', 'Ut', 'guangdong', 'eiusmod', 'http',NULL);
+INSERT INTO `user_info`(`uid`, `userName`, `password`, `email`, `phone`, `wechat`, `lastIP`, `regIP`, `role`,
+                        `nickName`,  `sex`, `city`, `country`, `province`, `language`, `headImgUrl`, `deletedTime`)
+VALUES (1740358057038188544, 'administrator', '4f0fded4a38abe7a3ea32f898bb82298', '163', '13911110000', 'wechat',
+        '127.0.0.1', '127.0.0.1', 1, 'liangjuan',
+        1, 'shenzhen', 'Ut', 'guangdong', 'eiusmod', 'http', NULL);
