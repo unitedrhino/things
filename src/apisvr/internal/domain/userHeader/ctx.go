eedd411e4f3f4df8865b424d0c6aa1a19c925b9a
lbs0019873800lbs <lbs_mike2006@163.com>
2022-09-05 15:57:15 +0800

    dev：从token中获取role信息

--- src/apisvr/internal/domain/userHeader/ctx.go
+++ src/apisvr/internal/domain/userHeader/ctx.go
@@ -3,12 +3,13 @@ package userHeader
 import "context"
 
 type UserCtx struct {
-	Uid int64  //用户id
-	IP  string //用户的ip地址
+	Uid  int64  //用户id
+	IP   string //用户的ip地址
+	Role int64  //用户角色
 }
 
-func SetUserCtx(ctx context.Context, uid int64, IP string) context.Context {
-	return context.WithValue(ctx, UserUid, &UserCtx{Uid: uid, IP: IP})
+func SetUserCtx(ctx context.Context, uid int64, IP string, role int64) context.Context {
+	return context.WithValue(ctx, UserUid, &UserCtx{Uid: uid, IP: IP, Role: role})
 }
 
 //使用该函数前必须传了UserCtx
