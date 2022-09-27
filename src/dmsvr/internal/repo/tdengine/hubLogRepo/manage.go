f3c15331a533303b01e2c0f7f423717c12f53730
godLei6 <603785348@qq.com>
2022-09-28 00:19:24 +0800

    dev: 完成事件日志开发及优化模型

--- src/dmsvr/internal/repo/tdengine/hubLogRepo/manage.go
+++ src/dmsvr/internal/repo/tdengine/hubLogRepo/manage.go
@@ -9,25 +9,21 @@ func (d HubLogRepo) InitProduct(ctx context.Context, productID string) error {
 	sql := fmt.Sprintf("CREATE STABLE IF NOT EXISTS %s "+
 		"(`ts` timestamp,`content` BINARY(5000),`topic` BINARY(500), `action` BINARY(100),"+
 		" `request_id` BINARY(100), `trance_id` BINARY(100), `result_type` BIGINT)"+
-		"TAGS (device_name BINARY(50));",
-		d.GetLogStableName(productID))
-	if _, err := d.t.ExecContext(ctx,sql); err != nil {
+		"TAGS (`product_id` BINARY(50),`device_name`  BINARY(50));",
+		d.GetLogStableName())
+	if _, err := d.t.ExecContext(ctx, sql); err != nil {
 		return err
 	}
 	return nil
 }
 
 func (d HubLogRepo) DropProduct(ctx context.Context, productID string) error {
-	sql := fmt.Sprintf("drop stable if exists %s;", d.GetLogStableName(productID))
-	if _, err := d.t.ExecContext(ctx,sql); err != nil {
-		return err
-	}
 	return nil
 }
 
 func (d HubLogRepo) DropDevice(ctx context.Context, productID string, deviceName string) error {
 	sql := fmt.Sprintf("drop table if exists %s;", d.GetLogTableName(productID, deviceName))
-	if _, err := d.t.ExecContext(ctx,sql); err != nil {
+	if _, err := d.t.ExecContext(ctx, sql); err != nil {
 		return err
 	}
 	return nil
