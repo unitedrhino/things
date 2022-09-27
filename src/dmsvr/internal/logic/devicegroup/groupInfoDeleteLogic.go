bf6bcf422dd6bddd7fac0bd6b9d5949620b49d16
lbs0019873800lbs <lbs_mike2006@163.com>
2022-09-27 12:04:52 +0800

    dev：增加错误码

--- src/dmsvr/internal/logic/devicegroup/groupInfoDeleteLogic.go
+++ src/dmsvr/internal/logic/devicegroup/groupInfoDeleteLogic.go
@@ -3,8 +3,6 @@ package devicegrouplogic
 import (
 	"context"
 	"github.com/i-Things/things/shared/errors"
-	"github.com/i-Things/things/src/dmsvr/internal/repo/mysql"
-
 	"github.com/i-Things/things/src/dmsvr/internal/svc"
 	"github.com/i-Things/things/src/dmsvr/pb/dm"
 
@@ -28,13 +26,9 @@ func NewGroupInfoDeleteLogic(ctx context.Context, svcCtx *svc.ServiceContext) *G
 // 删除分组
 func (l *GroupInfoDeleteLogic) GroupInfoDelete(in *dm.GroupInfoDeleteReq) (*dm.Response, error) {
 	//查询是否存在子组，若存在 则无法直接删除，返回删除失败
-	_, err := l.svcCtx.GroupInfo.FindOne(l.ctx, in.GroupID)
-	if err != nil {
-		if err == mysql.ErrNotFound {
-			return nil, errors.NotFind.AddDetailf("not find Group GroupID=%d",
-				in.GroupID)
-		}
-		return nil, errors.Database.AddDetail(err)
+	resp, err := l.svcCtx.GroupInfo.FindOne(l.ctx, in.GroupID)
+	if resp != nil {
+		return nil, errors.NotEmptyGroup.AddDetailf("the group have sun group can not delete.", in.GroupID)
 	}
 
 	//删除两表数据
