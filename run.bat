59ca2312f77db534ee3d3e9d67cfbe9d1c680f27
godLei6 <603785348@qq.com>
2022-10-09 20:25:00 +0800

    doc: 更新文档

--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
 rem 在windows下开发时，一键启动所有服务的脚本
-call :func_runsvr dmsvr dm.go
-call :func_runsvr disvr di.go
-call :func_runsvr syssvr sys.go
+rem call :func_runsvr dmsvr dm.go
+rem call :func_runsvr disvr di.go
+rem call :func_runsvr syssvr sys.go
 call :func_runsvr apisvr api.go
-call :func_runsvr ddsvr dd.go
+rem call :func_runsvr ddsvr dd.go
 
 :func_runsvr
 setlocal enabledelayedexpansion
