ebb5bb97306216cbe69aad260e5b8753a6816f9c
godLei6 <603785348@qq.com>
2022-10-16 20:55:30 +0800

    dev: 添加mqtt回调函数日志监控及修复utils.Fmt bug

--- shared/clients/mqtt.go
+++ shared/clients/mqtt.go
@@ -1,11 +1,14 @@
 package clients
 
 import (
+	"crypto/tls"
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/hashicorp/go-uuid"
 	"github.com/i-Things/things/shared/conf"
+	"github.com/i-Things/things/shared/utils"
 	"github.com/zeromicro/go-zero/core/logx"
+	"net/url"
 	"sync"
 	"time"
 )
@@ -31,6 +34,13 @@ func NewMqttClient(conf *conf.MqttConf) (mc mqtt.Client, err error) {
 			SetPassword(conf.Pass).SetAutoReconnect(true).SetConnectRetry(true)
 		opts.OnConnect = func(client mqtt.Client) {
 			logx.Info("mqtt client Connected")
+		}
+		opts.OnConnectAttempt = func(broker *url.URL, tlsCfg *tls.Config) *tls.Config {
+			logx.Infof("mqtt client connect attempt broker:%v", utils.Fmt(broker))
+			return tlsCfg
+		}
+		opts.OnConnectionLost = func(client mqtt.Client, err error) {
+			logx.Errorf("mqtt client connection lost err:%v", utils.Fmt(err))
 		}
 		mc := mqtt.NewClient(opts)
 		er2 := mc.Connect().WaitTimeout(5 * time.Second)
--- shared/utils/json.go
+++ shared/utils/json.go
@@ -19,6 +19,10 @@ func Fmt(v any) string {
 		return v.(string)
 	case []byte:
 		return string(v.([]byte))
+	case error:
+		return v.(error).Error()
+	case interface{ String() string }:
+		return v.(interface{ String() string }).String()
 	default:
 		js, err := json.Marshal(v)
 		if err != nil {
