096285067c62c53b27cee85cc8dd4b317ebef4b1
lbs0019873800lbs <lbs_mike2006@163.com>
2022-10-18 22:54:13 +0800

    dev：解决创建菜单遗漏插入关联菜单表问题

--- src/syssvr/internal/repo/mysql/menumodel.go
+++ src/syssvr/internal/repo/mysql/menumodel.go
@@ -6,12 +6,15 @@ import (
 	"github.com/zeromicro/go-zero/core/stores/cache"
 	"github.com/zeromicro/go-zero/core/stores/sqlc"
 	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"github.com/zeromicro/go-zero/core/stringx"
+	"strings"
 )
 
 type (
 	MenuModel interface {
 		Index(in *sys.MenuIndexReq) ([]*MenuInfo, error)
 		DeleteMenu(MenuId int64) error
+		InsertMenuID(data *MenuInfo, RoleId int64) error
 	}
 
 	menuModel struct {
@@ -20,6 +23,8 @@ type (
 		roleMenu string
 	}
 )
+
+var menuInfoRowsExpectAutoSet1 = strings.Join(stringx.Remove(menuInfoFieldNames, "`id`", "`updatedTime`", "`deletedTime`", "`createdTime`"), ",")
 
 func NewMenuModel(conn sqlx.SqlConn, c cache.CacheConf) MenuModel {
 	return &menuModel{
@@ -67,6 +72,28 @@ func (m *menuModel) DeleteMenu(MenuId int64) error {
 		//2.从角色菜单关系表删除关联菜单项
 		query = fmt.Sprintf("delete from %s where  menuID = %d",
 			m.roleMenu, MenuId)
+		_, err = session.Exec(query)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	return nil
+}
+
+func (m *menuModel) InsertMenuID(data *MenuInfo, RoleId int64) error {
+	m.Transact(func(session sqlx.Session) error {
+		//1.向menu_info表插入菜单项
+		query := fmt.Sprintf("insert into %s (%s) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", m.menuInfo, menuInfoRowsExpectAutoSet1)
+		ret, err := session.Exec(query, data.ParentID, data.Type, data.Order, data.Name, data.Path, data.Component, data.Icon, data.Redirect, data.BackgroundUrl, data.HideInMenu)
+		if err != nil {
+			return err
+		}
+		insertID, err := ret.LastInsertId()
+		//2.向role_menu表插入关联菜单项
+		query = fmt.Sprintf("insert into %s (roleID, menuID) values (%d, %d)",
+			m.roleMenu, RoleId, insertID)
 		_, err = session.Exec(query)
 		if err != nil {
 			return err
