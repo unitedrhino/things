0c746fe7ed255e8ca5f40fe74b9eefa2de18cd2b
godLei6 <603785348@qq.com>
2022-11-08 20:58:01 +0800

    dev: 整理代码

--- src/dmsvr/internal/repo/tdengine/sdkLogRepo/manage.go
+++ src/dmsvr/internal/repo/tdengine/sdkLogRepo/manage.go
@@ -10,7 +10,7 @@ import (
 func (d SDKLogRepo) InitProduct(ctx context.Context, productID string) error {
 	sql := fmt.Sprintf("CREATE STABLE IF NOT EXISTS %s "+
 		"(`ts` timestamp,`content` BINARY(5000),`logLevel` BINARY(100),`requestID` BINARY(100))"+
-		"TAGS (`productID` BINARY(50), deviceName BINARY(50));",
+		"TAGS (`productID` BINARY(50), `deviceName` BINARY(50));",
 		d.GetSDKLogStableName())
 	if _, err := d.t.ExecContext(ctx, sql); err != nil {
 		return err
