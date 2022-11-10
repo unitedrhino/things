9fc306ae4329173ad255a474e134496363bfe31a
godLei6 <603785348@qq.com>
2022-11-10 22:04:28 +0800

    fix: 修复sdkLog日志初始化sql bug

--- src/dmsvr/internal/repo/tdengine/sdkLogRepo/manage.go
+++ src/dmsvr/internal/repo/tdengine/sdkLogRepo/manage.go
@@ -9,8 +9,8 @@ import (
 
 func (d SDKLogRepo) InitProduct(ctx context.Context, productID string) error {
 	sql := fmt.Sprintf("CREATE STABLE IF NOT EXISTS %s "+
-		"(`ts` timestamp,`content` BINARY(5000),`logLevel` BINARY(100)"+
-		"TAGS (`productID` BINARY(50), `deviceName` BINARY(50));",
+		"(`ts` timestamp,`content` BINARY(5000),`logLevel` BINARY(100) "+
+		" TAGS (`productID` BINARY(50), `deviceName` BINARY(50));",
 		d.GetSDKLogStableName())
 	if _, err := d.t.ExecContext(ctx, sql); err != nil {
 		return err
