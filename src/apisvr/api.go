1787a7b99ef722e0c85d37827f6daf2e0d94b65e
godLei6 <603785348@qq.com>
2022-11-30 21:35:04 +0800

    dev: 完善外部二开iThings方案

--- src/apisvr/api.go
+++ src/apisvr/api.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/i-Things/things/src/apisvr/apidirect"
 	"github.com/zeromicro/go-zero/core/logx"
+	_ "github.com/zeromicro/go-zero/core/proc" //开启pprof采集 https://mp.weixin.qq.com/s/yYFM3YyBbOia3qah3eRVQA
 )
 
 func main() {
 	logx.DisableStat()
 	apiCtx := apidirect.NewApi(apidirect.ApiCtx{})
+	apiCtx.Server.PrintRoutes()
+	fmt.Printf("Starting apiSvr at %s:%d...\n", apiCtx.Svc.Config.Host, apiCtx.Svc.Config.Port)
 	apiCtx.Server.Start()
+	defer apiCtx.Server.Stop()
 }
