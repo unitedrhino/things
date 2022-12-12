e4eee27522847740dc80d19e471cf9a9861b90b9
godLei6 <603785348@qq.com>
2022-12-12 21:42:13 +0800

    dev: 开发完设备登录登出,属性及事件上报

--- src/disvr/internal/repo/tdengine/schemaDataRepo/assemble.go
+++ src/disvr/internal/repo/tdengine/schemaDataRepo/assemble.go
@@ -3,11 +3,11 @@ package schemaDataRepo
 import (
 	"encoding/json"
 	"github.com/i-Things/things/shared/domain/schema"
-	schema2 "github.com/i-Things/things/src/disvr/internal/domain/deviceMsg/msgThing"
+	"github.com/i-Things/things/src/disvr/internal/domain/deviceMsg/msgThing"
 	"github.com/spf13/cast"
 )
 
-func ToEventData(db map[string]any) *schema2.EventData {
+func ToEventData(db map[string]any) *msgThing.EventData {
 	var (
 		params   map[string]any
 		paramStr = cast.ToString(db["param"])
@@ -16,23 +16,23 @@ func ToEventData(db map[string]any) *schema2.EventData {
 	if err != nil {
 		return nil
 	}
-	data := schema2.EventData{
-		ID:        cast.ToString(db["eventID"]),
-		Type:      cast.ToString(db["eventType"]),
-		Params:    params,
-		TimeStamp: cast.ToTime(db["ts"]),
+	data := msgThing.EventData{
+		Identifier: cast.ToString(db["eventID"]),
+		Type:       cast.ToString(db["eventType"]),
+		Params:     params,
+		TimeStamp:  cast.ToTime(db["ts"]),
 	}
 	return &data
 }
 
-func ToPropertyData(id string, db map[string]any) *schema2.PropertyData {
+func ToPropertyData(id string, db map[string]any) *msgThing.PropertyData {
 	propertyType := db[PROPERTY_TYPE]
 	switch propertyType {
 	case string(schema.DataTypeStruct):
-		data := schema2.PropertyData{
-			ID:        id,
-			Param:     nil,
-			TimeStamp: cast.ToTime(db["ts"]),
+		data := msgThing.PropertyData{
+			Identifier: id,
+			Param:      nil,
+			TimeStamp:  cast.ToTime(db["ts"]),
 		}
 		delete(db, "ts")
 		delete(db, "deviceName")
@@ -43,17 +43,17 @@ func ToPropertyData(id string, db map[string]any) *schema2.PropertyData {
 		paramStr := cast.ToString(db["param"])
 		var param []any
 		json.Unmarshal([]byte(paramStr), &param)
-		data := schema2.PropertyData{
-			ID:        id,
-			Param:     param,
-			TimeStamp: cast.ToTime(db["ts"]),
+		data := msgThing.PropertyData{
+			Identifier: id,
+			Param:      param,
+			TimeStamp:  cast.ToTime(db["ts"]),
 		}
 		return &data
 	default:
-		data := schema2.PropertyData{
-			ID:        id,
-			Param:     cast.ToString(db["param"]),
-			TimeStamp: cast.ToTime(db["ts"]),
+		data := msgThing.PropertyData{
+			Identifier: id,
+			Param:      cast.ToString(db["param"]),
+			TimeStamp:  cast.ToTime(db["ts"]),
 		}
 		return &data
 	}
