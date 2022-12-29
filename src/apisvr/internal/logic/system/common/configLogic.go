6459554fa1a296db968e110c91c9bd82fb4af77e
lbs0019873800lbs <lbs_mike2006@163.com>
2022-12-29 17:04:08 +0800

    dev：补充漏传文件

---
+++ src/apisvr/internal/logic/system/common/configLogic.go
@@ -0,0 +1,37 @@
+package common
+
+import (
+	"context"
+	"github.com/i-Things/things/shared/errors"
+	"github.com/i-Things/things/shared/utils"
+	"github.com/i-Things/things/src/syssvr/pb/sys"
+
+	"github.com/i-Things/things/src/apisvr/internal/svc"
+	"github.com/i-Things/things/src/apisvr/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ConfigLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewConfigLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ConfigLogic {
+	return &ConfigLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *ConfigLogic) Config() (resp *types.ConfigResp, err error) {
+	rsp, err := l.svcCtx.SysConfig.Config(l.ctx, &sys.Response{})
+	if err != nil {
+		err = errors.Fmt(err)
+		l.Errorf("%s.rpc.SysConfig err=%+v", utils.FuncName(), err)
+		return nil, err
+	}
+	return &types.ConfigResp{Map: types.Map{Mode: rsp.Map.Mode, AccessKey: rsp.Map.AccessKey}}, nil
+}
---
+++ src/syssvr/internal/logic/config/configLogic.go
@@ -0,0 +1,30 @@
+package configlogic
+
+import (
+	"context"
+
+	"github.com/i-Things/things/src/syssvr/internal/svc"
+	"github.com/i-Things/things/src/syssvr/pb/sys"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ConfigLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewConfigLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ConfigLogic {
+	return &ConfigLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *ConfigLogic) Config(in *sys.Response) (*sys.ConfigResp, error) {
+	return &sys.ConfigResp{Map: &sys.Map{
+		Mode:      l.svcCtx.Config.Map.Mode,
+		AccessKey: l.svcCtx.Config.Map.AccessKey}}, nil
+}
