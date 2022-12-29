d923aa113f1c9a9716939c83d21746b32c8f3b7c
lbs0019873800lbs <lbs_mike2006@163.com>
2022-12-29 17:01:19 +0800

    dev：实现获取系统配置信息接口

--- src/syssvr/sysdirect/user.go
+++ src/syssvr/sysdirect/user.go
@@ -8,6 +8,9 @@ import (
 	clientRole "github.com/i-Things/things/src/syssvr/client/role"
 	serverMenu "github.com/i-Things/things/src/syssvr/internal/server/menu"
 	serverRole "github.com/i-Things/things/src/syssvr/internal/server/role"
+
+	clientConf "github.com/i-Things/things/src/syssvr/client/config"
+	serverConf "github.com/i-Things/things/src/syssvr/internal/server/config"
 )
 
 func NewUser() client.User {
@@ -23,4 +26,9 @@ func NewRole() clientRole.Role {
 func NewMenu() clientMenu.Menu {
 	userSvc := GetCtxSvc()
 	return clientMenu.NewDirectMenu(userSvc, serverMenu.NewMenuServer(userSvc))
+}
+
+func NewConfig() clientConf.Config {
+	userSvc := GetCtxSvc()
+	return clientConf.NewDirectConfig(userSvc, serverConf.NewConfigServer(userSvc))
 }
