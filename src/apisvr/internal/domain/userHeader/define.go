3193a5af1eb92a4cfb159ee579efeead4af55a24
godLei6 <603785348@qq.com>
2023-01-05 21:53:21 +0800

    dev: 添加开放认证能力

--- src/apisvr/internal/domain/userHeader/define.go
+++ src/apisvr/internal/domain/userHeader/define.go
@@ -1,9 +1,7 @@
 package userHeader
 
 const (
-	GUID         string = "iThings-guid"
 	UserToken    string = "iThings-token"
 	UserSetToken string = "iThings-set-token"
-	UserIp       string = "iThings-ip"
 	UserUid      string = "iThings-uid"
 )
--- src/dmsvr/internal/config/config.go
+++ src/dmsvr/internal/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"github.com/i-Things/things/shared/conf"
-	"github.com/i-Things/things/shared/domain/deviceAuth"
 	"github.com/zeromicro/go-zero/core/stores/cache"
 	"github.com/zeromicro/go-zero/zrpc"
 )
@@ -16,6 +15,6 @@ type Config struct {
 		DataSource string
 	}
 	CacheRedis cache.ClusterConf
-	Event      conf.EventConf       //和things内部交互的设置
-	AuthWhite  deviceAuth.AuthWhite //设备登录校验将things服务连接的ip设备root权限
+	Event      conf.EventConf //和things内部交互的设置
+	AuthWhite  conf.AuthConf  //设备登录校验将things服务连接的ip设备root权限
 }
