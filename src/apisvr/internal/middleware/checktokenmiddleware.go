004ce4ef5039ebbbda6ad3a93ea904bd88bcd54d
godLei6 <603785348@qq.com>
2023-01-05 21:53:21 +0800

    dev: 添加开放认证能力

--- src/apisvr/internal/middleware/checktokenmiddleware.go
+++ src/apisvr/internal/middleware/checktokenmiddleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"github.com/i-Things/things/shared/errors"
 	"github.com/i-Things/things/shared/utils"
+	"github.com/i-Things/things/src/apisvr/internal/config"
 	"github.com/i-Things/things/src/apisvr/internal/domain/userHeader"
 	user "github.com/i-Things/things/src/syssvr/client/user"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -11,40 +12,73 @@ import (
 
 type CheckTokenMiddleware struct {
 	UserRpc user.User
+	c       config.Config
 }
 
-func NewCheckTokenMiddleware(UserRpc user.User) *CheckTokenMiddleware {
-	return &CheckTokenMiddleware{UserRpc: UserRpc}
+func NewCheckTokenMiddleware(c config.Config, UserRpc user.User) *CheckTokenMiddleware {
+	return &CheckTokenMiddleware{UserRpc: UserRpc, c: c}
 }
 
 func (m *CheckTokenMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		strIP, _ := utils.GetIP(r)
-		strToken := r.Header.Get(userHeader.UserToken)
-		if strToken == "" {
-			logx.WithContext(r.Context()).Errorf("%s.CheckToken ip=%s not find token",
-				utils.FuncName(), strIP)
-			http.Error(w, errors.NotLogin.Error(), http.StatusUnauthorized)
+		err, isOpen := m.OpenAuth(w, r)
+		if isOpen { //如果是开放请求
+			if err == nil {
+				next(w, r)
+			} else {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+			}
 			return
 		}
-		resp, err := m.UserRpc.CheckToken(r.Context(), &user.CheckTokenReq{
-			Ip:    strIP,
-			Token: strToken,
-		})
-		if err != nil {
-			er := errors.Fmt(err)
-			logx.WithContext(r.Context()).Errorf("%s.CheckToken ip=%s token=%s return=%s",
-				utils.FuncName(), strIP, strToken, err)
-			http.Error(w, er.Error(), http.StatusUnauthorized)
+		userCtx, err := m.UserAuth(w, r)
+		if err == nil {
+			next(w, r.WithContext(userHeader.SetUserCtx(r.Context(), userCtx)))
 			return
 		}
-		if resp.Token != "" {
-			w.Header().Set("Access-Control-Expose-Headers", userHeader.UserSetToken)
-			w.Header().Set(userHeader.UserSetToken, resp.Token)
-		}
-		logx.WithContext(r.Context()).Infof("%s.CheckToken ip:%v in.token=%s checkResp:%v",
-			utils.FuncName(), strIP, strToken, utils.Fmt(resp))
-
-		next(w, r.WithContext(userHeader.SetUserCtx(r.Context(), 0, "", resp.Role)))
+		http.Error(w, err.Error(), http.StatusUnauthorized)
 	}
+}
+
+// 如果有开放认证的字段才进行认证
+func (m *CheckTokenMiddleware) OpenAuth(w http.ResponseWriter, r *http.Request) (error, bool) {
+	userName, password, ok := r.BasicAuth()
+	if !ok {
+		return nil, false
+	}
+	strIP, _ := utils.GetIP(r)
+	if !m.c.OpenAuth.Auth(userName, password, strIP) {
+		return errors.Permissions.AddMsg("开放认证没通过"), true
+	}
+	return nil, true
+}
+
+func (m *CheckTokenMiddleware) UserAuth(w http.ResponseWriter, r *http.Request) (*userHeader.UserCtx, error) {
+	strIP, _ := utils.GetIP(r)
+	strToken := r.Header.Get(userHeader.UserToken)
+	if strToken == "" {
+		logx.WithContext(r.Context()).Errorf("%s.CheckToken ip=%s not find token",
+			utils.FuncName(), strIP)
+		return nil, errors.NotLogin
+	}
+	resp, err := m.UserRpc.CheckToken(r.Context(), &user.CheckTokenReq{
+		Ip:    strIP,
+		Token: strToken,
+	})
+	if err != nil {
+		er := errors.Fmt(err)
+		logx.WithContext(r.Context()).Errorf("%s.CheckToken ip=%s token=%s return=%s",
+			utils.FuncName(), strIP, strToken, err)
+		return nil, er
+	}
+	if resp.Token != "" {
+		w.Header().Set("Access-Control-Expose-Headers", userHeader.UserSetToken)
+		w.Header().Set(userHeader.UserSetToken, resp.Token)
+	}
+	logx.WithContext(r.Context()).Infof("%s.CheckToken ip:%v in.token=%s checkResp:%v",
+		utils.FuncName(), strIP, strToken, utils.Fmt(resp))
+	return &userHeader.UserCtx{
+		Uid:  resp.Uid,
+		IP:   strIP,
+		Role: resp.Role,
+	}, nil
 }
