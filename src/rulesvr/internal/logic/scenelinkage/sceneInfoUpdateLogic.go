98c774a7aa0bc64ce9b7d504dd5bf9721fda5fb6
godLei6 <603785348@qq.com>
2023-02-15 23:20:55 +0800

    dev: 场景联动 - 完成接口打通

---
+++ src/rulesvr/internal/logic/scenelinkage/sceneInfoUpdateLogic.go
@@ -0,0 +1,30 @@
+package scenelinkagelogic
+
+import (
+	"context"
+
+	"github.com/i-Things/things/src/rulesvr/internal/svc"
+	"github.com/i-Things/things/src/rulesvr/pb/rule"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type SceneInfoUpdateLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewSceneInfoUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *SceneInfoUpdateLogic {
+	return &SceneInfoUpdateLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *SceneInfoUpdateLogic) SceneInfoUpdate(in *rule.SceneInfo) (*rule.Response, error) {
+	// todo: add your logic here and delete this line
+
+	return &rule.Response{}, nil
+}
