5e2a683ed8ce3a2a1af3f031ef4bad7a6a0631e1
godLei6 <603785348@qq.com>
2023-02-22 21:31:37 +0800

    dev: 完善场景联动增删查改

--- src/rulesvr/internal/domain/scene/scene.go
+++ src/rulesvr/internal/domain/scene/scene.go
@@ -1,22 +1,37 @@
 package scene
 
 import (
+	"github.com/i-Things/things/shared/def"
+	"github.com/i-Things/things/shared/errors"
+	"github.com/i-Things/things/shared/utils"
 	"time"
 )
 
+type TriggerType string
+
+const (
+	TriggerTypeDevice TriggerType = "device"
+	TriggerTypeTimer  TriggerType = "timer"
+	TriggerTypeManual TriggerType = "manual"
+)
+
 type Info struct {
-	ID          int64     `json:"id"`
-	Name        string    `json:"name"`
-	Desc        string    `json:"desc"`
-	CreatedTime time.Time `json:"createdTime"`
-	Trigger     Trigger   `json:"trigger"`
-	When        Terms     `json:"when"` //只有设备触发时才有用
-	Then        Actions   `json:"then"`
-	Status      int64     `json:"status"` // 状态
+	ID          int64       `json:"id"`
+	Name        string      `json:"name"`
+	Desc        string      `json:"desc"`
+	CreatedTime time.Time   `json:"createdTime"`
+	TriggerType TriggerType `json:"triggerType"` //触发类型 device: 设备触发 timer: 定时触发 manual:手动触发
+	Trigger     Trigger     `json:"trigger"`
+	When        Terms       `json:"when"` //只有设备触发时才有用
+	Then        Actions     `json:"then"`
+	State       int64       `json:"state"` // 状态（1启用 2禁用）
 }
 
 func (i *Info) Validate() error {
-	err := i.Trigger.Validate()
+	if !utils.SliceIn(i.TriggerType, TriggerTypeDevice) {
+		return errors.Parameter.AddMsg("触发器不支持的类型:" + string(i.TriggerType))
+	}
+	err := i.Trigger.Validate(i.TriggerType)
 	if err != nil {
 		return err
 	}
@@ -27,6 +42,9 @@ func (i *Info) Validate() error {
 	err = i.Then.Validate()
 	if err != nil {
 		return err
+	}
+	if i.State == 0 {
+		i.State = def.Enable
 	}
 	return nil
 }
--- src/rulesvr/internal/domain/scene/trigger.go
+++ src/rulesvr/internal/domain/scene/trigger.go
@@ -3,31 +3,23 @@ package scene
 
 import (
 	"github.com/i-Things/things/shared/errors"
-	"github.com/i-Things/things/shared/utils"
-)
-
-type TriggerType string
-
-const (
-	TriggerTypeDevice TriggerType = "device"
-	TriggerTypeTimer  TriggerType = "timer"
-	TriggerTypeManual TriggerType = "manual"
 )
 
 type Trigger struct {
-	Type   TriggerType    `json:"type"` //触发类型 device: 设备触发 timer: 定时触发 manual:手动触发
 	Device *TriggerDevice `json:"device"`
 }
 
-func (t *Trigger) Validate() error {
+func (t *Trigger) Validate(triggerType TriggerType) error {
 	if t == nil {
 		return nil
 	}
-	if !utils.SliceIn(t.Type, TriggerTypeDevice) {
-		return errors.Parameter.AddMsg("触发器不支持的类型:" + string(t.Type))
+
+	switch triggerType {
+	case TriggerTypeDevice:
+		if t.Device == nil {
+			return errors.Parameter.AddMsg("设备类型触发器需要填写触发详情")
+		}
+		return t.Device.Validate()
 	}
-	if t.Device == nil {
-		return errors.Parameter.AddMsg("设备类型触发器需要填写触发详情")
-	}
-	return t.Device.Validate()
+	return nil
 }
