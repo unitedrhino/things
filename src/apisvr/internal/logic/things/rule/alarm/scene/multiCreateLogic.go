f024c6cc57308a2afa9dbd5c604724459bc6d97b
godLei6 <603785348@qq.com>
2023-03-22 22:25:17 +0800

    dev: 告警中心 - 接口开发3.22

--- src/apisvr/internal/logic/things/rule/alarm/scene/multiCreateLogic.go
+++ src/apisvr/internal/logic/things/rule/alarm/scene/multiCreateLogic.go
@@ -2,6 +2,9 @@ package scene
 
 import (
 	"context"
+	"github.com/i-Things/things/shared/errors"
+	"github.com/i-Things/things/shared/utils"
+	"github.com/i-Things/things/src/rulesvr/pb/rule"
 
 	"github.com/i-Things/things/src/apisvr/internal/svc"
 	"github.com/i-Things/things/src/apisvr/internal/types"
@@ -24,7 +27,14 @@ func NewMultiCreateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Multi
 }
 
 func (l *MultiCreateLogic) MultiCreate(req *types.AlarmSceneMultiCreateReq) error {
-	// todo: add your logic here and delete this line
-
+	_, err := l.svcCtx.Alarm.AlarmSceneMultiCreate(l.ctx, &rule.AlarmSceneMultiCreateReq{
+		AlarmID:  req.AlarmID,
+		SceneIDs: req.SceneIDs,
+	})
+	if err != nil {
+		er := errors.Fmt(err)
+		l.Errorf("%s.rpc.AlarmSceneMultiCreate req=%v err=%v", utils.FuncName(), req, er)
+		return er
+	}
 	return nil
 }
