1c7159fe9765a559678c1d57ee8e9e1d874adb48
robotyang <robotyang123@163.com>
2023-04-03 23:54:20 +0800

    feat(设备管理): 批量导入
    - 调通 接收和遍历csv骨架

--- src/apisvr/internal/handler/things/device/info/multiImportHandler.go
+++ src/apisvr/internal/handler/things/device/info/multiImportHandler.go
@@ -8,16 +8,42 @@ import (
 	"github.com/i-Things/things/src/apisvr/internal/types"
 	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
+	"path"
 )
 
 func MultiImportHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req types.DeviceMultiImportReq
-		//f, fh, err := r.FormFile("file")
 		if err := httpx.Parse(r, &req); err != nil {
 			result.Http(w, r, nil, errors.Parameter.WithMsg("入参不正确:"+err.Error()))
 			return
 		}
+
+		f, fh, err := r.FormFile("file")
+		if err != nil {
+			result.Http(w, r, nil, errors.Parameter.WithMsg("请上传文件:"+err.Error()))
+			return
+		}
+		defer f.Close()
+
+		//判断和限制格式
+		if ext := path.Ext(fh.Filename); ext != ".csv" {
+			result.Http(w, r, nil, errors.Parameter.WithMsg("仅支持csv文件"))
+			return
+		}
+
+		//判断和限制大小
+		if KB := float64(fh.Size) / 1024; KB > 700 { //byte->KB，限制最大700KB
+			result.Http(w, r, nil, errors.Parameter.WithMsg("文件不能超过700KB"))
+			return
+		}
+
+		fb := make([]byte, fh.Size)
+		if _, err = f.Read(fb); err != nil {
+			result.Http(w, r, nil, errors.Parameter.WithMsg("解析文件字节失败:"+err.Error()))
+			return
+		}
+		req.File = fb
 
 		l := info.NewMultiImportLogic(r.Context(), svcCtx)
 		resp, err := l.MultiImport(&req)
--- src/apisvr/internal/logic/things/device/info/multiImportLogic.go
+++ src/apisvr/internal/logic/things/device/info/multiImportLogic.go
@@ -1,12 +1,13 @@
 package info
 
 import (
+	"bytes"
 	"context"
-
 	"github.com/i-Things/things/src/apisvr/internal/svc"
 	"github.com/i-Things/things/src/apisvr/internal/types"
-
+	"github.com/xuri/excelize/v2"
 	"github.com/zeromicro/go-zero/core/logx"
+	"golang.org/x/sync/errgroup"
 )
 
 type MultiImportLogic struct {
@@ -24,7 +25,42 @@ func NewMultiImportLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Multi
 }
 
 func (l *MultiImportLogic) MultiImport(req *types.DeviceMultiImportReq) (resp *types.DeviceMultiImportResp, err error) {
-	// todo: add your logic here and delete this line
+	reader := bytes.NewReader(req.File)
+	csv, err := excelize.OpenReader(reader)
+	if err != nil {
+		return nil, err
+	}
+	rows, err := csv.Rows("Sheet1")
+	if err != nil {
+		return nil, err
+	}
+
+	var egg errgroup.Group
+	var errDatas []types.DeviceMultiImportErrdata
+
+	for r := int64(0); rows.Next(); r++ {
+		if cell, err := rows.Columns(); err != nil {
+			errDatas = append(errDatas, types.DeviceMultiImportErrdata{
+				Row: r,
+				Msg: "读取行数据出错:" + err.Error(),
+			})
+		} else {
+			if r < 2 { //第1行是标题、第2行是示例，均跳过
+				continue
+			}
+
+			//TODO 校验行单元格数据
+
+			egg.Go(func() error {
+				_ = cell //TODO 异步添加设备逻辑
+				return nil
+			})
+		}
+	}
+	//阻塞等待所有gorouting
+	if err = egg.Wait(); err != nil {
+		return nil, err
+	}
 
 	return
 }
