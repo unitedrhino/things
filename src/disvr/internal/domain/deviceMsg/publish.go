94a590b453b986e77051b5b2cd115c1854e5eb9e
yangl30 <yangl30@mingyuanyun.com>
2023-04-04 17:28:25 +0800

    dev: 网关改造 - 功能开发完毕,尚未测试

--- src/disvr/internal/domain/deviceMsg/publish.go
+++ src/disvr/internal/domain/deviceMsg/publish.go
@@ -29,12 +29,16 @@ type (
 	}
 
 	CommonMsg struct { //消息内容通用字段
-		Method      string `json:"method"`              //操作方法
-		ClientToken string `json:"clientToken"`         //方便排查随机数
-		Timestamp   int64  `json:"timestamp,omitempty"` //毫秒时间戳
-		Code        int64  `json:"code,omitempty"`      //状态码
-		Status      string `json:"status,omitempty"`    //返回信息
-		Data        any    `json:"data,omitempty"`      //返回具体设备上报的最新数据内容
+		Method      string    `json:"method"`              //操作方法
+		ClientToken string    `json:"clientToken"`         //方便排查随机数
+		Timestamp   int64     `json:"timestamp,omitempty"` //毫秒时间戳
+		Code        int64     `json:"code,omitempty"`      //状态码
+		Status      string    `json:"status,omitempty"`    //返回信息
+		Data        any       `json:"data,omitempty"`      //返回具体设备上报的最新数据内容
+		Sys         SysConfig `json:"sys,omitempty"`       //系统配置
+	}
+	SysConfig struct {
+		NoAsk bool `json:"noAsk"` //云平台是否回复消息
 	}
 )
 
@@ -60,6 +64,9 @@ func NewRespCommonMsg(method, clientToken string) *CommonMsg {
 		ClientToken: clientToken,
 		Timestamp:   time.Now().UnixMilli(),
 	}
+}
+func (c *CommonMsg) NoAsk() bool {
+	return c.Sys.NoAsk
 }
 
 func (c *CommonMsg) GetTimeStamp() time.Time {
