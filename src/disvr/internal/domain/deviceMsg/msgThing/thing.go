5da3a368a3d4b94b6b5fd396f193465a1c3e7c69
robotyang <robotyang123@163.com>
2023-04-12 00:01:23 +0800

    feat(物模型): 设备基础信息上报
    - 增加 代码注释

--- src/disvr/internal/domain/deviceMsg/msgThing/thing.go
+++ src/disvr/internal/domain/deviceMsg/msgThing/thing.go
@@ -1,9 +1,9 @@
 package msgThing
 
 const (
-	TypeProperty = "property"
-	TypeEvent    = "event"
-	TypeAction   = "action"
+	TypeProperty = "property" //事件-操作类型：属性或信息上报
+	TypeEvent    = "event"    //事件-操作类型：事件上报
+	TypeAction   = "action"   //事件-操作类型：行为上报
 
 	EventReport = "report" //设备上报的信息
 	EventInfo   = "info"   //信息
