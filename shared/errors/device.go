fb836728fbe4ebe8ccab607ab5cbe47bd8746589
yangl30 <yangl30@mingyuanyun.com>
2023-04-14 15:24:35 +0800

    fix: 修复下发给设备消息的时候没有校验是否在线

--- shared/errors/device.go
+++ shared/errors/device.go
@@ -5,4 +5,5 @@ const DEVICE_ERROR = 2000000
 var (
 	RespParam     = NewCodeError(DEVICE_ERROR+1, "返回参数不对")
 	DeviceTimeOut = NewCodeError(DEVICE_ERROR+2, "设备回复超时")
+	NotOnline     = NewCodeError(DEVICE_ERROR+3, "设备不在线")
 )
