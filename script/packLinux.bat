fcaf0fc326a10a623a92e976b221194d110f92d4
godLei6 <603785348@qq.com>
2023-05-10 21:04:18 +0800

    dev: 更新sql

--- script/packLinux.bat
+++ script/packLinux.bat
@@ -1,18 +1,5 @@
 SET GOOS=linux
 SET GOARCH=amd64
-cd ../src/apisvr
-rem "update backend"
-go mod tidy
-go build api.go
-mkdir -p  ..\..\pack\linux
-cp -rf api dist etc ../../pack/linux
-cd ../../script
-rem "update front"
-call buildFront.bat
-cp -rf ../assets/dist/* ../pack/linux/dist/front/iThingsCore
-cd ../pack
-rm -rf iThings-linux.tgz
-tar -zcvf iThings-linux.tgz ./linux/*
-cd ../script
+call pack.bat
 SET GOOS=windows
 SET GOARCH=amd64
