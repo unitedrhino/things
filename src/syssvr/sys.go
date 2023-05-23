518df268918796b245bab89945d8c3c9c19a3b92
godLei6 <603785348@qq.com>
2023-05-23 18:12:13 +0800

    dev: 添加开始的时候recover

--- src/syssvr/sys.go
+++ src/syssvr/sys.go
@@ -1,11 +1,14 @@
-//系统管理模块-syssvr
+// 系统管理模块-syssvr
 package main
 
 import (
+	"context"
+	"github.com/i-Things/things/shared/utils"
 	"github.com/i-Things/things/src/syssvr/sysdirect"
 )
 
 func main() {
+	defer utils.Recover(context.Background())
 	svcCtx := sysdirect.GetSvcCtx()
 	sysdirect.RunServer(svcCtx)
 }
