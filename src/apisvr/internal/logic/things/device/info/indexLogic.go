23e49cc0d56641661f91db6a4d628b48ed30b548
lbs0019873800lbs <lbs_mike2006@163.com>
2023-05-24 22:16:14 +0800

    dev：设备别名支持查询

--- src/apisvr/internal/logic/things/device/info/indexLogic.go
+++ src/apisvr/internal/logic/things/device/info/indexLogic.go
@@ -29,13 +29,18 @@ func NewIndexLogic(ctx context.Context, svcCtx *svc.ServiceContext) *IndexLogic 
 }
 
 func (l *IndexLogic) Index(req *types.DeviceInfoIndexReq) (resp *types.DeviceInfoIndexResp, err error) {
+	var deviceAlias string = ""
+	if req.DeviceAlias != nil {
+		deviceAlias = *req.DeviceAlias
+	}
 	dmReq := &dm.DeviceInfoIndexReq{
-		ProductID:  req.ProductID, //产品id
-		DeviceName: req.DeviceName,
-		Tags:       logic.ToTagsMap(req.Tags),
-		Page:       logic.ToDmPageRpc(req.Page),
-		Range:      req.Range,
-		Position:   logic.ToDmPointRpc(req.Position),
+		ProductID:   req.ProductID, //产品id
+		DeviceName:  req.DeviceName,
+		Tags:        logic.ToTagsMap(req.Tags),
+		Page:        logic.ToDmPageRpc(req.Page),
+		Range:       req.Range,
+		Position:    logic.ToDmPointRpc(req.Position),
+		DeviceAlias: deviceAlias,
 	}
 	dmResp, err := l.svcCtx.DeviceM.DeviceInfoIndex(l.ctx, dmReq)
 	if err != nil {
