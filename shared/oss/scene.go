228e02388b912b680b79cfc8698a1f3f49168408
godLei6 <603785348@qq.com>
2023-05-25 11:48:41 +0800

    feat: 调整代码格式

--- shared/oss/scene.go
+++ shared/oss/scene.go
@@ -24,13 +24,6 @@ const (
 	SceneProductImg       = "productImg"    //产品图片
 )
 
-var (
-	//Key 是业务 value是场景
-	SceneRecord = map[string]map[string]struct{}{
-		BusinessProductManage: {SceneProductImg: struct{}{}},
-	}
-)
-
 func GetSceneInfo(filePath string) (*SceneInfo, error) {
 	paths := strings.Split(filePath, "/")
 	if len(paths) < 3 {
@@ -42,18 +35,7 @@ func GetSceneInfo(filePath string) (*SceneInfo, error) {
 		FilePath: strings.Join(paths[2:], "/"),
 		FileName: paths[len(paths)-1],
 	}
-	return scene, CheckSceneInfo(scene)
-}
-
-func CheckSceneInfo(info *SceneInfo) error {
-	scene := SceneRecord[info.Business]
-	if scene == nil {
-		return errors.Parameter.WithMsgf("business is not right:%v", info.Business)
-	}
-	if _, ok := scene[info.Scene]; !ok {
-		return errors.Parameter.WithMsgf("scene is not right:%v", info.Scene)
-	}
-	return nil
+	return scene, nil
 }
 
 func GetFilePath(scene *SceneInfo, rename bool) (string, error) {
@@ -69,7 +51,7 @@ func GetFilePath(scene *SceneInfo, rename bool) (string, error) {
 		}
 		scene.FilePath = uuid + ext
 	} else {
-		spcChar := []string{`,`, `?`, `*`, `|`, `{`, `}`, `\`, `/`, `$`, `、`, `·`, "`", `'`, `"`}
+		spcChar := []string{`,`, `?`, `*`, `|`, `{`, `}`, `\`, `$`, `、`, `·`, "`", `'`, `"`}
 		if strings.ContainsAny(scene.FilePath, strings.Join(spcChar, "")) {
 			return "", errors.Parameter.WithMsg("包含特殊字符")
 		}
