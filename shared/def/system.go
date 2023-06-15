5c69d771e32dd8e13b85e69880d64d33a1d71b9a
godLei6 <603785348@qq.com>
2023-06-15 16:24:29 +0800

    feat: 合并最新代码

--- shared/def/system.go
+++ shared/def/system.go
@@ -1,9 +1,5 @@
 package def
 
-import (
-	"github.com/i-Things/things/shared/store"
-)
-
 const DeviceGroupLevel = 3
 
 type RoleID int64
@@ -27,17 +23,4 @@ type Point struct {
 	CoordinateSystem CoordinateSystem `json:"coordinateSystem,omitempty"` //坐标系：WGS84(地球系)，GCJ02(火星系)，BD09(百度系)<br/>参考解释：https://www.cnblogs.com/bigroc/p/16423120.html
 	Longitude        float64          `json:"longitude,range=[0:180]"`    //经度
 	Latitude         float64          `json:"latitude,range=[0:90]"`      //纬度
-}
-
-func (p Point) ToPo() store.Point {
-	return store.Point{
-		Longitude: p.Longitude,
-		Latitude:  p.Latitude,
-	}
-}
-func ToPoint(p store.Point) Point {
-	return Point{
-		Longitude: p.Longitude,
-		Latitude:  p.Latitude,
-	}
 }
