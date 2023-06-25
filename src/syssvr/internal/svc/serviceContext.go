6c291779f660a5983f9c8933214f95dfa35559cc
godLei6 <603785348@qq.com>
2023-06-25 15:23:49 +0800

    feat: 规范化小程序

--- src/syssvr/internal/svc/serviceContext.go
+++ src/syssvr/internal/svc/serviceContext.go
@@ -1,9 +1,10 @@
 package svc
 
 import (
+	"context"
 	"github.com/casbin/casbin/v2"
 	cas "github.com/i-Things/things/shared/casbin"
-	"github.com/i-Things/things/shared/third/weixin"
+	"github.com/i-Things/things/shared/clients"
 	"github.com/i-Things/things/shared/utils"
 	"github.com/i-Things/things/src/syssvr/internal/config"
 	"github.com/i-Things/things/src/syssvr/internal/repo/mysql"
@@ -14,13 +15,13 @@ import (
 type ServiceContext struct {
 	Config        config.Config
 	UserInfoModel mysql.SysUserInfoModel
-	RoleInfoModle mysql.SysRoleInfoModel
-	MenuInfoModle mysql.SysMenuInfoModel
-	RoleMenuModle mysql.SysRoleMenuModel
+	RoleInfoModel mysql.SysRoleInfoModel
+	MenuInfoModel mysql.SysMenuInfoModel
+	RoleMenuModel mysql.SysRoleMenuModel
 	UserModel     mysql.UserModel
 	RoleModel     mysql.RoleModel
 	MenuModel     mysql.MenuModel
-	WxMiniProgram *weixin.MiniProgram
+	WxMiniProgram *clients.MiniProgram
 	UserID        *utils.SnowFlake
 	LogLoginModel mysql.SysLoginLogModel
 	LogOperModel  mysql.SysOperLogModel
@@ -43,11 +44,11 @@ func NewServiceContext(c config.Config) *ServiceContext {
 	mem := mysql.NewMenuModel(conn, c.CacheRedis)
 	um := mysql.NewUserModel(conn, c.CacheRedis)
 
-	WxMiniProgram := weixin.NewWexinMiniProgram(c.WexinMiniprogram, c.CacheRedis)
+	WxMiniProgram := clients.NewWxMiniProgram(context.Background(), c.WxMiniProgram, c.CacheRedis)
 	nodeId := utils.GetNodeID(c.CacheRedis, c.Name)
 	UserID := utils.NewSnowFlake(nodeId)
 	db, _ := conn.RawDB()
-	ca := cas.NewCasbinWithRedisWatcher(db, "mysql", c.CacheRedis[0].RedisConf)
+	ca := cas.NewCasbinWithRedisWatcher(db, c.Database.DBType, c.CacheRedis[0].RedisConf)
 	store := kv.NewStore(c.CacheRedis)
 
 	return &ServiceContext{
@@ -56,8 +57,8 @@ func NewServiceContext(c config.Config) *ServiceContext {
 		UserModel:     um,
 		RoleModel:     rom,
 		MenuModel:     mem,
-		RoleInfoModle: ro,
-		MenuInfoModle: me,
+		RoleInfoModel: ro,
+		MenuInfoModel: me,
 		WxMiniProgram: WxMiniProgram,
 		UserID:        UserID,
 		LogLoginModel: ll,
