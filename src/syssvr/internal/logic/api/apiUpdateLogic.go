f3b0647629f77877d2b2af81974531f69ab1df79
godLei6 <603785348@qq.com>
2023-07-07 22:25:25 +0800

    feat: gorm切换 - syssvr初步重构完成

--- src/syssvr/internal/logic/api/apiUpdateLogic.go
+++ src/syssvr/internal/logic/api/apiUpdateLogic.go
@@ -3,7 +3,7 @@ package apilogic
 import (
 	"context"
 	"github.com/i-Things/things/shared/errors"
-	"github.com/i-Things/things/src/syssvr/internal/repo/mysql"
+	"github.com/i-Things/things/src/syssvr/internal/repo/relationDB"
 
 	"github.com/i-Things/things/src/syssvr/internal/svc"
 	"github.com/i-Things/things/src/syssvr/pb/sys"
@@ -15,6 +15,7 @@ type ApiUpdateLogic struct {
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
 	logx.Logger
+	AiDB *relationDB.ApiInfoRepo
 }
 
 func NewApiUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ApiUpdateLogic {
@@ -22,17 +23,18 @@ func NewApiUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ApiUpda
 		ctx:    ctx,
 		svcCtx: svcCtx,
 		Logger: logx.WithContext(ctx),
+		AiDB:   relationDB.NewApiInfoRepo(ctx),
 	}
 }
 
 func (l *ApiUpdateLogic) ApiUpdate(in *sys.ApiUpdateReq) (*sys.Response, error) {
-	res, err := l.svcCtx.ApiModel.FindOne(l.ctx, in.Id)
+	res, err := l.AiDB.FindOne(l.ctx, in.Id)
 	if err != nil {
 		return nil, errors.Database.AddDetail(err)
 	}
 
-	err = l.svcCtx.ApiModel.Update(l.ctx, &mysql.SysApiInfo{
-		Id:           in.Id,
+	err = l.AiDB.Update(l.ctx, &relationDB.SysApiInfo{
+		ID:           in.Id,
 		Route:        in.Route,
 		Method:       in.Method,
 		Name:         in.Name,
