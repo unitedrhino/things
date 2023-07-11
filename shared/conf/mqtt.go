fd937e59833419cd5b8d0ba590c72c65315eb678
godLei6 <603785348@qq.com>
2023-07-11 16:04:13 +0800

    feat: 添加mqtt连接池

--- shared/conf/mqtt.go
+++ shared/conf/mqtt.go
@@ -5,4 +5,5 @@ type MqttConf struct {
 	Brokers  []string //mqtt服务器节点
 	User     string   `json:",default=root"` //用户名
 	Pass     string   `json:",optional"`     //密码
+	ConnNum  int      `json:",default=10"`   //默认连接数
 }
