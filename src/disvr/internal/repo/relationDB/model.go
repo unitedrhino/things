5144ab0c42a7491c8d425a20f6bd91086dcb1c53
yeahbull <237401117@qq.com>
2023-07-20 21:25:22 +0800

    调整pg数据库字段命名规则

--- src/disvr/internal/repo/relationDB/model.go
+++ src/disvr/internal/repo/relationDB/model.go
@@ -8,11 +8,11 @@ import (
 // 设备影子表
 type DiDeviceShadow struct {
 	ID                int64      `gorm:"column:id;type:bigint(20);primary_key;AUTO_INCREMENT"`
-	ProductID         string     `gorm:"column:productID;type:char(11);NOT NULL"`             // 产品id
-	DeviceName        string     `gorm:"column:deviceName;type:varchar(100);NOT NULL"`        // 设备名称
-	DataID            string     `gorm:"column:dataID;type:varchar(100);NOT NULL"`            // 属性id
-	Value             string     `gorm:"column:value;type:varchar(100);default:NULL"`         // 属性值
-	UpdatedDeviceTime *time.Time `gorm:"column:updatedDeviceTime;type:datetime;default:NULL"` //更新到设备时间
+	ProductID         string     `gorm:"column:product_id;type:char(11);NOT NULL"`              // 产品id
+	DeviceName        string     `gorm:"column:device_name;type:varchar(100);NOT NULL"`         // 设备名称
+	DataID            string     `gorm:"column:data_id;type:varchar(100);NOT NULL"`             // 属性id
+	Value             string     `gorm:"column:value;type:varchar(100);default:NULL"`           // 属性值
+	UpdatedDeviceTime *time.Time `gorm:"column:updated_device_time;type:datetime;default:NULL"` //更新到设备时间
 	stores.Time
 }
 
