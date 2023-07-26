e551f055292f0b990ad938498b44c290a01808f7
godLei6 <603785348@qq.com>
2023-07-26 12:52:10 +0800

    feat: 建表支持mysql及索引

--- src/disvr/internal/repo/relationDB/model.go
+++ src/disvr/internal/repo/relationDB/model.go
@@ -8,14 +8,17 @@ import (
 // 设备影子表
 type DiDeviceShadow struct {
 	ID                int64      `gorm:"column:id;type:BIGINT;primary_key;AUTO_INCREMENT"`
-	ProductID         string     `gorm:"column:product_id;type:CHAR(11);NOT NULL"`
-	DeviceName        string     `gorm:"column:device_name;type:VARCHAR(100);NOT NULL"`
-	DataID            string     `gorm:"column:data_id;type:VARCHAR(100);NOT NULL"`
+	ProductID         string     `gorm:"column:product_id;uniqueIndex:pi_dn_di;type:CHAR(11);NOT NULL"`
+	DeviceName        string     `gorm:"column:device_name;uniqueIndex:pi_dn_di;type:VARCHAR(100);NOT NULL"`
+	DataID            string     `gorm:"column:data_id;uniqueIndex:pi_dn_di;type:VARCHAR(100);NOT NULL"`
 	Value             string     `gorm:"column:value;type:VARCHAR(100);default:NULL"`
-	UpdatedDeviceTime *time.Time `gorm:"column:updated_device_time;type:timestamp without time zone;default:NULL"`
+	UpdatedDeviceTime *time.Time `gorm:"column:updated_device_time;default:NULL"`
 	stores.Time
 }
 
 func (m *DiDeviceShadow) TableName() string {
+	return "di_device_shadow"
+}
+func (m *DiDeviceShadow) Comment() string {
 	return "di_device_shadow"
 }
