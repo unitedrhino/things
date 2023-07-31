c6a391db649077b867373e16aabc64f1c201179d
godLei6 <603785348@qq.com>
2023-07-31 19:50:32 +0800

    feat: 修复字段名错误

--- src/dmsvr/internal/repo/tdengine/sdkLogRepo/manage.go
+++ src/dmsvr/internal/repo/tdengine/sdkLogRepo/manage.go
@@ -9,7 +9,7 @@ import (
 
 func (d SDKLogRepo) InitProduct(ctx context.Context, productID string) error {
 	sql := fmt.Sprintf("CREATE STABLE IF NOT EXISTS %s "+
-		"(`ts` timestamp,`content` BINARY(5000),`logLevel` BINARY(100)) "+
+		"(`ts` timestamp,`content` BINARY(5000),`log_level` BINARY(100)) "+
 		" TAGS (`product_id` BINARY(50), `device_name` BINARY(50));",
 		d.GetSDKLogStableName())
 	if _, err := d.t.ExecContext(ctx, sql); err != nil {
