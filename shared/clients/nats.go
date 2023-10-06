6e7032afbb8f91b21a3388fd4a759a08a3edba6e
godLei6 <603785348@qq.com>
2023-10-06 21:33:50 +0800

    feat: 支持Nats 的 stream模式

--- shared/clients/nats.go
+++ shared/clients/nats.go
@@ -26,3 +26,41 @@ func NewNatsClient(conf conf.NatsConf) (nc *nats.Conn, err error) {
 	}
 	return nc, err
 }
+
+func NewNatsJetStreamClient(conf conf.NatsConf) (nats.JetStreamContext, error) {
+	nc, err := NewNatsClient(conf)
+	if err != nil {
+		return nil, err
+	}
+	return nc.JetStream(nats.PublishAsyncMaxPending(256))
+}
+
+func CreateStream(jetStream nats.JetStreamContext, name string, subjects []string) error {
+	stream, err := jetStream.StreamInfo(name)
+	// stream not found, create it
+	if stream == nil {
+		_, err = jetStream.AddStream(&nats.StreamConfig{
+			Name:     name,
+			Subjects: subjects,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func CreateConsumer(jetStream nats.JetStreamContext, stream, name string) error {
+	ConsumerInfo, err := jetStream.ConsumerInfo(stream, name)
+	// stream not found, create it
+	if ConsumerInfo == nil {
+		_, err = jetStream.AddConsumer(stream, &nats.ConsumerConfig{
+			Durable:        name,
+			AckPolicy:      nats.AckAllPolicy,
+			DeliverSubject: nats.NewInbox(),
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
