7f3b66e940b2a59611f7b9b04b2c16b682245662
godLei6 <603785348@qq.com>
2023-10-14 22:45:13 +0800

    feat: 支持过期时间

--- shared/timedDelay/timed.go
+++ shared/timedDelay/timed.go
@@ -4,7 +4,16 @@ import (
 	"github.com/hibiken/asynq"
 	"github.com/i-Things/things/shared/clients"
 	"github.com/zeromicro/go-zero/core/stores/cache"
+	"time"
 )
+
+type Option struct {
+	//以下两个参数优先使用ProcessIn
+	ProcessIn time.Duration //多久之后发
+	ProcessAt time.Time     // 固定时间发
+	Timeout   time.Duration //超时时间 优先使用
+	Deadline  time.Time     //截止时间
+}
 
 type Timed struct {
 	asynqClient *asynq.Client
