8a898aa77bb788b24b1db3dcc987964c4a20fc3d
godLei6 <603785348@qq.com>
2023-10-23 11:21:50 +0800

    feat: 添加新服务编译

--- shared/timedDelay/sql.go
+++ shared/timedDelay/sql.go
@@ -7,7 +7,7 @@ import (
 )
 
 type SqlMsg struct {
-	Type   string // sql执行的类型  normal(支持环境变量的直接执行方式),JavaScript(js脚本执行)
+	Type   string // sql执行的类型  normal(支持环境变量的直接执行方式),js(js脚本执行)
 	Sql    string // 执行的sql
 	Script string `json:"script"`
 	//需要使用的环境变量 dsn:如果填写,默认使用该dsn来连接,dbType:mysql|pgsql 默认是mysql
