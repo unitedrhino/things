903834a69239b15a8f0632d05b254c708ea723fb
godLei6 <603785348@qq.com>
2023-11-30 19:24:47 +0800

    feat: 将tdengine调整为默认websocket连接模式

--- shared/clients/tdEngine.go
+++ shared/clients/tdEngine.go
@@ -2,13 +2,14 @@ package clients
 
 import (
 	"database/sql"
+	"github.com/i-Things/things/shared/conf"
 	_ "github.com/taosdata/driver-go/v3/taosRestful"
+	_ "github.com/taosdata/driver-go/v3/taosWS"
 	"github.com/zeromicro/go-zero/core/logx"
 	"sync"
 )
 
 type Td struct {
-	Dsn string
 	*sql.DB
 }
 
@@ -17,14 +18,17 @@ var (
 	once = sync.Once{}
 )
 
-func NewTDengine(DataSource string) (TD *Td, err error) {
+func NewTDengine(DataSource conf.TSDB) (TD *Td, err error) {
 	once.Do(func() {
-		td.DB, err = sql.Open("taosRestful", DataSource)
+		driver := "taosWS"
+		if DataSource.Driver == "taosRestful" {
+			driver = "taosRestful"
+		}
+		td.DB, err = sql.Open(driver, DataSource.DSN)
 		if err != nil {
 			return
 		}
-		td.Dsn = DataSource
-		_, err = td.Exec("create database if not exists iThings;")
+		_, err = td.Exec("create database if not exists ithings;")
 	})
 	if err != nil {
 		logx.Errorf("tdengine 初始化失败,err:%v", err)
