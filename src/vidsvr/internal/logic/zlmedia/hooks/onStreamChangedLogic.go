fd43bad3e99fe3c338be818ad8d4515474329371
aigens <wfjdemmye@163.com>
2023-12-13 15:12:46 +0800

    style: 删除无用注释和代码(不影响逻辑)

--- src/vidsvr/internal/logic/zlmedia/hooks/onStreamChangedLogic.go
+++ src/vidsvr/internal/logic/zlmedia/hooks/onStreamChangedLogic.go
@@ -3,7 +3,6 @@ package hooks
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/i-Things/things/shared/errors"
 	"github.com/i-Things/things/shared/utils"
 	"github.com/i-Things/things/src/vidsvr/internal/repo/relationDB"
@@ -14,37 +13,22 @@ import (
 	"time"
 )
 
-const StreamUpTimeOut = 2 * time.Second
-
 type OnStreamChangedLogic struct {
 	logx.Logger
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
-	//cache  cache.Cache
 }
-
-//var (
-//	// can't use one SharedCalls per conn, because multiple conns may share the same cache key.
-//	exclusiveCalls = syncx.NewSingleFlight()
-//	stats          = cache.NewStat("streamupdate")
-//	cachePrefix    = "streamupdate"
-//)
 
 func NewOnStreamChangedLogic(ctx context.Context, svcCtx *svc.ServiceContext) *OnStreamChangedLogic {
 	return &OnStreamChangedLogic{
 		Logger: logx.WithContext(ctx),
 		ctx:    ctx,
 		svcCtx: svcCtx,
-		//cache:  cache.New(svcCtx.Config.CacheRedis, exclusiveCalls, stats, errors.NotFind),
 	}
 }
 
-// [fix-bug by wfj]   不同IP推上来的相同路径的流，可能会有问题
 func (l *OnStreamChangedLogic) OnStreamChanged(req *types.HooksApiStreamChangedRep) (resp *types.HooksApiResp, err error) {
 	// todo: add your logic here and delete this line
-	reqStr, _ := json.Marshal(*req)
-	fmt.Println("---------OnStreamChanged--------------:", string(reqStr))
-
 	//需要先判断该流服务是否有注册过，未注册过忽略消息
 	infoRepo := relationDB.NewVidmgrInfoRepo(l.ctx)
 	vidInfo, err := infoRepo.FindOneByFilter(l.ctx, relationDB.VidmgrFilter{
@@ -77,11 +61,9 @@ func (l *OnStreamChangedLogic) OnStreamChanged(req *types.HooksApiStreamChangedR
 				vidStreamInfo.FirstLogin = time.Now()
 				vidStreamInfo.LastLogin = time.Now()
 				//不关心流类型了
-				//SetProtocol(req.Schema, vidStreamInfo)
 				if vidStreamInfo.OriginType == RTMP_PUSH {
 					re := regexp.MustCompile(vidStreamInfo.Vhost)
 					if vidInfo.MediasvrType == 1 { //docker 模式
-						//l.svcCtx.Config.Restconf.Host
 						vidStreamInfo.OriginUrl =
 							re.ReplaceAllString(vidStreamInfo.OriginUrl, l.svcCtx.Config.Restconf.Host)
 					} else {
@@ -99,17 +81,12 @@ func (l *OnStreamChangedLogic) OnStreamChanged(req *types.HooksApiStreamChangedR
 				return nil, err
 			}
 		} else { //找到了一条流就直需要修改状态就可以了
-			//判断Sock相同为同一流  	update
 			if req.Regist {
-				//对应位(bit)  置1
-				//SetProtocol(req.Schema, vidStreamIndex)
 				vidStreamInfo.IsOnline = true
 				vidStreamInfo.LastLogin = time.Now()
 			} else {
-				//对应位(bit) 置0
 				vidStreamInfo.IsOnline = false
 				vidStreamInfo.LastLogin = time.Now()
-				//UnSetProtocol(req.Schema, vidStreamInfo)
 			}
 			err := streamRepo.Update(l.ctx, vidStreamInfo)
 			if err != nil {
