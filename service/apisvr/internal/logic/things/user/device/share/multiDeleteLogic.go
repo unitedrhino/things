281b16ac422ded6a803b36f8331f7c53d065614a
godLei6 <603785348@qq.com>
2024-04-13 14:06:04 +0800

    feat: 完善项目

---
+++ service/apisvr/internal/logic/things/user/device/share/multiDeleteLogic.go
@@ -0,0 +1,31 @@
+package share
+
+import (
+	"context"
+	"gitee.com/i-Things/share/utils"
+	"github.com/i-Things/things/service/dmsvr/pb/dm"
+
+	"github.com/i-Things/things/service/apisvr/internal/svc"
+	"github.com/i-Things/things/service/apisvr/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type MultiDeleteLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewMultiDeleteLogic(ctx context.Context, svcCtx *svc.ServiceContext) *MultiDeleteLogic {
+	return &MultiDeleteLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *MultiDeleteLogic) MultiDelete(req *types.UserDeviceShareMultiDeleteReq) error {
+	_, err := l.svcCtx.UserDevice.UserDeviceShareMultiDelete(l.ctx, utils.Copy[dm.UserDeviceShareMultiDeleteReq](req))
+	return err
+}
---
+++ service/dmsvr/internal/logic/userdevice/userDeviceShareMultiDeleteLogic.go
@@ -0,0 +1,45 @@
+package userdevicelogic
+
+import (
+	"context"
+	"gitee.com/i-Things/core/service/syssvr/pb/sys"
+	"gitee.com/i-Things/share/ctxs"
+	"gitee.com/i-Things/share/errors"
+	"github.com/i-Things/things/service/dmsvr/internal/repo/relationDB"
+
+	"github.com/i-Things/things/service/dmsvr/internal/svc"
+	"github.com/i-Things/things/service/dmsvr/pb/dm"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserDeviceShareMultiDeleteLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewUserDeviceShareMultiDeleteLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserDeviceShareMultiDeleteLogic {
+	return &UserDeviceShareMultiDeleteLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// 取消分享设备
+func (l *UserDeviceShareMultiDeleteLogic) UserDeviceShareMultiDelete(in *dm.UserDeviceShareMultiDeleteReq) (*dm.Empty, error) {
+	pi, err := l.svcCtx.ProjectM.ProjectInfoRead(l.ctx, &sys.ProjectWithID{ProjectID: int64(in.ProjectID)})
+	if err != nil {
+		return nil, err
+	}
+	uc := ctxs.GetUserCtx(l.ctx)
+	if pi.AdminUserID != uc.UserID { //只有所有者和被分享者才有权限操作
+		return nil, errors.Permissions
+	}
+	err = relationDB.NewUserDeviceShareRepo(l.ctx).DeleteByFilter(l.ctx, relationDB.UserDeviceShareFilter{
+		ProjectID: in.ProjectID,
+		IDs:       in.Ids,
+	})
+	return &dm.Empty{}, err
+}
