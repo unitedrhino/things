fe73165ddfde4190fbfadadc33c93f0fd94a0298
godLei6 <603785348@qq.com>
2024-05-09 12:07:23 +0800

    feat: 完善项目

--- service/dmsvr/internal/event/deviceMsgEvent/deviceMsg.go
+++ service/dmsvr/internal/event/deviceMsgEvent/deviceMsg.go
@@ -3,9 +3,15 @@ package deviceMsgEvent
 //设备的发布,连接及断连处理
 import (
 	"context"
+	"gitee.com/i-Things/share/ctxs"
+	"gitee.com/i-Things/share/def"
+	"gitee.com/i-Things/share/devices"
+	"gitee.com/i-Things/share/domain/application"
 	"gitee.com/i-Things/share/domain/deviceMsg"
 	"gitee.com/i-Things/share/utils"
+	"github.com/i-Things/things/service/dmsvr/dmExport"
 	"github.com/i-Things/things/service/dmsvr/internal/domain/deviceStatus"
+	"github.com/i-Things/things/service/dmsvr/internal/repo/relationDB"
 	"github.com/i-Things/things/service/dmsvr/internal/svc"
 	"github.com/zeromicro/go-zero/core/logx"
 	"time"
@@ -27,6 +33,7 @@ func NewDeviceMsgHandle(ctx context.Context, svcCtx *svc.ServiceContext) *Device
 
 func (l *DeviceMsgHandle) Gateway(msg *deviceMsg.PublishMsg) error {
 	l.Infof("%s req=%v", utils.FuncName(), msg)
+	l.FixDisconnect(msg)
 	resp, err := NewGatewayLogic(l.ctx, l.svcCtx).Handle(msg)
 	l.deviceResp(resp)
 	l.Infof("%s req:%v resp:%v err:%v", utils.FuncName(), msg, resp, err)
@@ -35,6 +42,7 @@ func (l *DeviceMsgHandle) Gateway(msg *deviceMsg.PublishMsg) error {
 
 func (l *DeviceMsgHandle) Thing(msg *deviceMsg.PublishMsg) error {
 	l.Infof("%s req=%v", utils.FuncName(), msg)
+	l.FixDisconnect(msg)
 	startTime := time.Now()
 	resp, err := NewThingLogic(l.ctx, l.svcCtx).Handle(msg)
 	l.deviceResp(resp)
@@ -45,6 +53,7 @@ func (l *DeviceMsgHandle) Thing(msg *deviceMsg.PublishMsg) error {
 
 func (l *DeviceMsgHandle) Ota(msg *deviceMsg.PublishMsg) error {
 	l.Infof("%s req=%v", utils.FuncName(), msg)
+	l.FixDisconnect(msg)
 	resp, err := NewOtaLogic(l.ctx, l.svcCtx).Handle(msg)
 	l.deviceResp(resp)
 	l.Infof("%s req:%v resp:%v err:%v", utils.FuncName(), msg, resp, err)
@@ -53,6 +62,7 @@ func (l *DeviceMsgHandle) Ota(msg *deviceMsg.PublishMsg) error {
 
 func (l *DeviceMsgHandle) Shadow(msg *deviceMsg.PublishMsg) error {
 	l.Infof("%s req=%v", utils.FuncName(), msg)
+	l.FixDisconnect(msg)
 	resp, err := NewShadowLogic(l.ctx, l.svcCtx).Handle(msg)
 	l.deviceResp(resp)
 	l.Infof("%s req:%v resp:%v err:%v", utils.FuncName(), msg, resp, err)
@@ -61,6 +71,7 @@ func (l *DeviceMsgHandle) Shadow(msg *deviceMsg.PublishMsg) error {
 
 func (l *DeviceMsgHandle) Config(msg *deviceMsg.PublishMsg) error {
 	l.Infof("%s req=%v", utils.FuncName(), msg)
+	l.FixDisconnect(msg)
 	respMsg, err := NewConfigLogic(l.ctx, l.svcCtx).Handle(msg)
 	l.deviceResp(respMsg)
 	l.Infof("%s req:%v resp:%v err:%v", utils.FuncName(), msg, respMsg, err)
@@ -69,6 +80,7 @@ func (l *DeviceMsgHandle) Config(msg *deviceMsg.PublishMsg) error {
 
 func (l *DeviceMsgHandle) Ext(msg *deviceMsg.PublishMsg) error {
 	l.Infof("%s req=%v", utils.FuncName(), msg)
+	l.FixDisconnect(msg)
 	resp, err := NewExtLogic(l.ctx, l.svcCtx).Handle(msg)
 	l.deviceResp(resp)
 	l.Infof("%s req:%v resp:%v err:%v", utils.FuncName(), msg, resp, err)
@@ -76,6 +88,7 @@ func (l *DeviceMsgHandle) Ext(msg *deviceMsg.PublishMsg) error {
 }
 
 func (l *DeviceMsgHandle) SDKLog(msg *deviceMsg.PublishMsg) error {
+	l.FixDisconnect(msg)
 	respMsg, err := NewSDKLogLogic(l.ctx, l.svcCtx).Handle(msg)
 	l.deviceResp(respMsg)
 	l.Infof("%s req:%v resp:%v err:%v", utils.FuncName(), msg, respMsg, err)
@@ -102,4 +115,41 @@ func (l *DeviceMsgHandle) deviceResp(respMsg *deviceMsg.PublishMsg) {
 		return
 	}
 	l.WithDuration(time.Now().Sub(startTime)).Infof("DeviceMsgHandle.deviceResp startTime:%v msg:%v", startTime, respMsg)
+}
+func (l *DeviceMsgHandle) FixDisconnect(msg *deviceMsg.PublishMsg) {
+	ctxs.GoNewCtx(l.ctx, func(ctx context.Context) {
+		di, err := l.svcCtx.DeviceCache.GetData(ctx, dmExport.GenDeviceInfoKey(msg.ProductID, msg.DeviceName))
+		if err != nil {
+			l.Error(err)
+			return
+		}
+		if di.IsOnline != def.True { //如果不在线但是上报了需要调整为在线状态
+			var updates = map[string]any{"is_online": def.True, "last_login": msg.Timestamp, "status": def.DeviceStatusOnline}
+			if di.FirstLogin == 0 {
+				updates["first_login"] = msg.Timestamp
+			}
+			err = relationDB.NewDeviceInfoRepo(ctx).UpdateWithField(ctx,
+				relationDB.DeviceFilter{Cores: []*devices.Core{{ProductID: msg.ProductID, DeviceName: msg.DeviceName}}}, updates)
+			if err != nil {
+				l.Error(err)
+			}
+			err = l.svcCtx.DeviceCache.SetData(ctx, dmExport.GenDeviceInfoKey(msg.ProductID, msg.DeviceName), nil)
+			if err != nil {
+				l.Error(err)
+			}
+			appMsg := application.ConnectMsg{
+				Device: devices.Core{
+					ProductID:  msg.ProductID,
+					DeviceName: msg.DeviceName,
+				},
+				Status:    def.ConnectedStatus,
+				Timestamp: msg.Timestamp,
+			}
+			err = l.svcCtx.PubApp.DeviceStatusConnected(ctx, appMsg)
+			if err != nil {
+				l.Errorf("%s.pubApp productID:%v deviceName:%v err:%v",
+					utils.FuncName(), msg.ProductID, msg.DeviceName, err)
+			}
+		}
+	})
 }
