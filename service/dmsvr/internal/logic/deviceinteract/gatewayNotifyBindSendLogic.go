809ded5d3c4afe38c42f8da5d83bf1d5c8fccd19
godLei6 <603785348@qq.com>
2024-05-20 16:25:00 +0800

    feat: 完善项目

--- service/dmsvr/internal/logic/deviceinteract/gatewayNotifyBindSendLogic.go
+++ service/dmsvr/internal/logic/deviceinteract/gatewayNotifyBindSendLogic.go
@@ -6,6 +6,9 @@ import (
 	"gitee.com/i-Things/share/devices"
 	"gitee.com/i-Things/share/domain/deviceMsg"
 	"gitee.com/i-Things/share/domain/deviceMsg/msgGateway"
+	"gitee.com/i-Things/share/domain/deviceMsg/msgThing"
+	"gitee.com/i-Things/share/errors"
+	"gitee.com/i-Things/share/utils"
 	"time"
 
 	"github.com/i-Things/things/service/dmsvr/internal/svc"
@@ -66,28 +69,28 @@ func (l *GatewayNotifyBindSendLogic) GatewayNotifyBindSend(in *dm.GatewayNotifyB
 		ProtocolCode: protocolCode,
 	}
 	err = l.svcCtx.PubDev.PublishToDev(l.ctx, &reqMsg)
-	//var resp []byte
-	//resp, err = l.svcCtx.PubDev.ReqToDeviceSync(l.ctx, &reqMsg, func(payload []byte) bool {
-	//	var dresp msgThing.Resp
-	//	err = utils.Unmarshal(payload, &dresp)
-	//	if err != nil { //如果是没法解析的说明不是需要的包,直接跳过即可
-	//		return false
-	//	}
-	//	if dresp.MsgToken != req.MsgToken { //不是该请求的回复.跳过
-	//		return false
-	//	}
-	//	return true
-	//})
-	//if err != nil {
-	//	return nil, err
-	//}
-	//var dresp msgGateway.Msg
-	//err = utils.Unmarshal(resp, &dresp)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//if dresp.Code != errors.OK.GetCode() {
-	//	return nil, errors.DeviceResp.AddMsgf("设备返回错误: msg:%v code:%v", dresp.Msg, dresp.Code)
-	//}
+	var resp []byte
+	resp, err = l.svcCtx.PubDev.ReqToDeviceSync(l.ctx, &reqMsg, func(payload []byte) bool {
+		var dresp msgThing.Resp
+		err = utils.Unmarshal(payload, &dresp)
+		if err != nil { //如果是没法解析的说明不是需要的包,直接跳过即可
+			return false
+		}
+		if dresp.MsgToken != req.MsgToken { //不是该请求的回复.跳过
+			return false
+		}
+		return true
+	})
+	if err != nil {
+		return nil, err
+	}
+	var dresp msgGateway.Msg
+	err = utils.Unmarshal(resp, &dresp)
+	if err != nil {
+		return nil, err
+	}
+	if dresp.Code != errors.OK.GetCode() {
+		return nil, errors.DeviceResp.AddMsgf("设备返回错误: msg:%v code:%v", dresp.Msg, dresp.Code)
+	}
 	return &dm.Empty{}, err
 }
