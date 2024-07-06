b1d98a63daab6724f4945eb076bb73167877ac63
godLei6 <603785348@qq.com>
2024-07-06 08:52:22 +0800

    feat: 完善项目

--- service/dmsvr/internal/event/staticEvent/static.go
+++ service/dmsvr/internal/event/staticEvent/static.go
@@ -2,10 +2,13 @@ package staticEvent
 
 import (
 	"context"
+	"gitee.com/i-Things/core/service/syssvr/pb/sys"
 	"gitee.com/i-Things/share/utils"
+	"github.com/i-Things/things/service/dmsvr/internal/logic"
 	"github.com/i-Things/things/service/dmsvr/internal/repo/relationDB"
 	"github.com/i-Things/things/service/dmsvr/internal/svc"
 	"github.com/zeromicro/go-zero/core/logx"
+	"sync"
 )
 
 type StaticHandle struct {
@@ -23,6 +26,37 @@ func NewStaticHandle(ctx context.Context, svcCtx *svc.ServiceContext) *StaticHan
 }
 
 func (l *StaticHandle) Handle() error { //产品品类设备数量统计
+	w := sync.WaitGroup{}
+	w.Add(2)
+	utils.Go(l.ctx, func() {
+		err := l.ProductCategoryStatic()
+		if err != nil {
+			l.Error(err)
+		}
+	})
+	utils.Go(l.ctx, func() {
+		err := l.AreaDeviceStatic()
+		if err != nil {
+			l.Error(err)
+		}
+	})
+	w.Wait()
+	return nil
+}
+func (l *StaticHandle) AreaDeviceStatic() error { //区域下的设备数量统计
+	ret, err := l.svcCtx.AreaM.AreaInfoIndex(l.ctx, &sys.AreaInfoIndexReq{})
+	if err != nil {
+		return err
+	}
+	var areaPaths []string
+	for _, v := range ret.List {
+		areaPaths = append(areaPaths, v.AreaIDPath)
+	}
+	err = logic.FillAreaDeviceCount(l.ctx, l.svcCtx, areaPaths...)
+	return err
+}
+
+func (l *StaticHandle) ProductCategoryStatic() error { //产品品类设备数量统计
 	pcDB := relationDB.NewProductCategoryRepo(l.ctx)
 	pcs, err := pcDB.FindByFilter(l.ctx, relationDB.ProductCategoryFilter{}, nil)
 	if err != nil {
