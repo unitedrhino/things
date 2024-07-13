259eb5346eccfa2ab754f72f033a5c50f023938e
godLei6 <603785348@qq.com>
2024-07-13 18:42:44 +0800

    feat: 完善项目

--- service/udsvr/ud.go
+++ service/udsvr/ud.go
@@ -3,26 +3,24 @@ package main
 import (
 	"flag"
 	"fmt"
+	"gitee.com/i-Things/share/utils"
 
 	"github.com/i-Things/things/service/udsvr/internal/config"
 	ruleServer "github.com/i-Things/things/service/udsvr/internal/server/rule"
 	"github.com/i-Things/things/service/udsvr/internal/svc"
 	"github.com/i-Things/things/service/udsvr/pb/ud"
 
-	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/core/service"
 	"github.com/zeromicro/go-zero/zrpc"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
-var configFile = flag.String("f", "etc/ud.yaml", "the config file")
-
 func main() {
 	flag.Parse()
 
 	var c config.Config
-	utils.ConfMustLoad(*configFile, &c)
+	utils.ConfMustLoad("etc/ud.yaml", &c)
 	ctx := svc.NewServiceContext(c)
 
 	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
