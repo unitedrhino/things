b65e2603dbb0bf304be3471fddbd4fbd247e3d3d
godLei6 <603785348@qq.com>
2024-10-09 20:53:02 +0800

    feat: 网关上线子设备自动订阅子设备

--- service/dgsvr/internal/event/innerSub/innerSub.go
+++ service/dgsvr/internal/event/innerSub/innerSub.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 	"gitee.com/i-Things/share/devices"
+	"gitee.com/i-Things/share/domain/deviceMsg"
+	"gitee.com/i-Things/share/domain/deviceMsg/msgGateway"
+	"gitee.com/i-Things/share/errors"
 	"gitee.com/i-Things/share/utils"
 	"gitee.com/i-Things/things/service/dgsvr/internal/domain/custom"
 	"gitee.com/i-Things/things/service/dgsvr/internal/svc"
@@ -26,6 +29,12 @@ func NewInnerSubServer(svcCtx *svc.ServiceContext, ctx context.Context) *InnerSu
 
 func (s *InnerSubServer) PublishToDev(info *devices.InnerPublish) error {
 	var finalPayload = info.Payload
+	if info.Handle == devices.Gateway && info.Type == msgGateway.TypeStatus { //网关类型如果是操作子设备需要调整topic绑定状态
+		err := s.GatewayHandle(info)
+		if err != nil {
+			s.Error(err)
+		}
+	}
 	topic := fmt.Sprintf("%s/down/%s/%s/%s", "$"+info.Handle, info.Type, info.ProductID, info.DeviceName)
 
 	f, err := s.svcCtx.Script.GetProtoFunc(s.ctx, info.ProductID, custom.ConvertTypeDown, info.Handle, info.Type)
@@ -45,4 +54,28 @@ func (s *InnerSubServer) PublishToDev(info *devices.InnerPublish) error {
 	}
 
 	return s.svcCtx.PubDev.Publish(s.ctx, topic, finalPayload)
+}
+func (s *InnerSubServer) GatewayHandle(info *devices.InnerPublish) error {
+	var resp msgGateway.Msg
+	err := utils.Unmarshal(info.Payload, &resp)
+	if err != nil {
+		return errors.Parameter.AddDetailf("payload unmarshal payload:%v err:%v", string(info.Payload), err)
+	}
+	var topics []string
+	if resp.Payload != nil && len(resp.Payload.Devices) != 0 {
+		for _, v := range resp.Payload.Devices {
+			topics = append(topics,
+				fmt.Sprintf("$thing/down/property/%s/%s", v.ProductID, v.DeviceName),
+				fmt.Sprintf("$thing/down/event/%s/%s", v.ProductID, v.DeviceName),
+				fmt.Sprintf("$thing/down/action/%s/%s", v.ProductID, v.DeviceName),
+				fmt.Sprintf("$ota/down/upgrade/%s/%s", v.ProductID, v.DeviceName))
+		}
+	}
+	clientID := fmt.Sprintf("%s&%s", info.ProductID, info.DeviceName)
+	if resp.Method == deviceMsg.Online {
+		err = s.svcCtx.MqttClient.SetClientMutSub(s.ctx, clientID, topics, 1)
+	} else {
+		err = s.svcCtx.MqttClient.SetClientMutUnSub(s.ctx, clientID, topics)
+	}
+	return err
 }
