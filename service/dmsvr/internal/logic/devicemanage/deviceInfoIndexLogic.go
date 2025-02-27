14afe262b59aaa552c226eb28bc94f5df3ded425
godLei6 <603785348@qq.com>
2025-02-27 21:34:08 +0800

    fix: 修复信号过滤

--- service/dmsvr/internal/logic/devicemanage/deviceInfoIndexLogic.go
+++ service/dmsvr/internal/logic/devicemanage/deviceInfoIndexLogic.go
@@ -94,7 +94,7 @@ func (l *DeviceInfoIndexLogic) DeviceInfoIndex(in *dm.DeviceInfoIndexReq) (*dm.D
 		filter.ExpTime = stores.GetCmp(in.ExpTime.CmpType, cast.ToTime(in.ExpTime.Value))
 	}
 	if in.Rssi != nil {
-		filter.Rssi = stores.GetCmp(in.Rssi.CmpType, cast.ToTime(in.Rssi.Value))
+		filter.Rssi = stores.GetCmp(in.Rssi.CmpType, in.Rssi.Value)
 	}
 	if err := ctxs.IsRoot(l.ctx); err == nil { //default租户才可以查看其他租户的设备
 		ctxs.GetUserCtx(l.ctx).AllTenant = true
